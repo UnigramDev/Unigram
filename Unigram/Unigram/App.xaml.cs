414163bc6ef17043f402d43d39a9a59f2865f592
Amank053 <amank053@gmail.com>
2016-05-24 23:36:53 +0530

    [Dev-Few Xaml Changes- Aman]

--- Unigram/Unigram/App.xaml.cs
+++ Unigram/Unigram/App.xaml.cs
@@ -75,7 +75,7 @@ namespace Unigram
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(Views.ChatsSummary), e.Arguments);
+                    rootFrame.Navigate(typeof(Views.ChatHistory), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
