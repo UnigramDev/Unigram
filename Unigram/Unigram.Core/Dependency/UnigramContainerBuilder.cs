c3ddf67efda4c2b4b49f08c42cef45af6134d03d
Luis Guerrero <guerrerotook@gmail.com>
2016-05-26 11:36:47 +0200

    Added Bootstrapper Class and registration in the Autofac ContainerBuilder to get the Instagram API

---
+++ Unigram/Unigram.Core/Dependency/UnigramContainerBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace Unigram.Core.Dependency
+{
+    using Autofac;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class UnigramContainerBuilder : ContainerBuilder
+    {
+        private static UnigramContainerBuilder instance = new UnigramContainerBuilder();
+
+        private UnigramContainerBuilder()
+        {
+        }
+
+        public static UnigramContainerBuilder Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
