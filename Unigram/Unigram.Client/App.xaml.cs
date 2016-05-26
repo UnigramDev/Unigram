2dd6a36daf2f4de57fae802d68e741f2e25d0789
ikarago <ikarago@outlook.com>
2016-05-26 11:49:07 +0200

    Removed AppInsights and set Startup-page of the Client to Views/Home.xaml

--- Unigram/Unigram.Client/App.xaml.cs
+++ Unigram/Unigram.Client/App.xaml.cs
@@ -29,9 +29,6 @@ namespace Unigram.Client
         /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
@@ -76,7 +73,7 @@ namespace Unigram.Client
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Views.Home), e.Arguments);
                 }
 
                 // Ensure the current window is active
