708e5e7b98017350f96ce6db14e739757bfbca32
Luis Guerrero <guerrerotook@gmail.com>
2016-05-26 11:00:32 +0200

    Merge Mockups brachn to develop

---
+++ Unigram/Unigram.Client/Views/ChatsSummary.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Unigram.Client.Views
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.InteropServices.WindowsRuntime;
+    using Windows.Foundation;
+    using Windows.Foundation.Collections;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Controls.Primitives;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Navigation;
+
+    public sealed partial class ChatsSummary : Page
+    {
+        public ChatsSummary()
+        {
+            this.InitializeComponent();
+        }
+
+        private void cbtnMasterAbout_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Views.About));
+        }
+    }
+}
