ddcc1b75379a3028f549f37087aa4fbbea9d7015
Luis Guerrero <guerrerotook@gmail.com>
2016-05-26 10:22:27 +0200

    Added the Unigram.Core proyect and the StyleCop.Analyzer nuget package for code style.

---
+++ Unigram/Unigram.ViewModel/UnigramViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Unigram.ViewModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Base class for ViewModel
+    /// </summary>
+    public class UnigramViewModel
+    {
+
+    }
+}
