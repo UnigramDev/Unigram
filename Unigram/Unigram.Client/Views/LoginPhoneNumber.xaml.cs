a98a9cd098dfdbcb90e9d2e2d6b9588042a49ea5
ikarago <ikarago@outlook.com>
2016-05-26 14:21:06 +0200

    Added transistions to Login-pages

--- Unigram/Unigram.Client/Views/LoginPhoneNumber.xaml.cs
+++ Unigram/Unigram.Client/Views/LoginPhoneNumber.xaml.cs
@@ -26,5 +26,10 @@ namespace Unigram.Client.Views
         {
             this.InitializeComponent();
         }
+
+        private void btnMasterPhoneInputConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Views.LoginPhoneCode));
+        }
     }
 }
