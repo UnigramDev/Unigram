213562359bc55f572d80d7c48665d26139ca4bcc
Fela Ameghino <frayx@live.it>
2016-06-26 21:21:52 +0200

    Starting from scratch.

---
+++ Unigram/Unigram.Api/Constants.Secret.cs
@@ -0,0 +1,14 @@
+﻿namespace Telegram.Api
+{
+    public static partial class Constants
+    {
+        static Constants()
+        {
+#error PLACE YOUR DATA
+            ApiId = 0;
+            ApiHash = "";
+
+            FirstServerIpAddress = "";
+        }
+    }
+}
