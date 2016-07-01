2709fb5aa1ea1aeb7995577c8b5ccb1c87ebcd8f
ikarago <ikarago@outlook.com>
2016-07-01 21:54:40 +0200

    Various updates to the ShareTargetPage + support for sharing URL's added

---
+++ Unigram/Unigram/Helpers/ShareTargetHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.ApplicationModel.DataTransfer.ShareTarget;
+
+namespace Unigram.Helpers
+{
+    public class ShareTargetHelper
+    {
+        ShareOperation shareOperation;
+        public string sharedTitle;
+        public string sharedDescription;
+        public string sharedText;
+        public Uri sharedWebLink;
+
+
+
+        public ShareTargetHelper(ShareOperation newShareOperation)
+        {
+            shareOperation = newShareOperation;
+            this.GetStuffFromContract();
+
+        }
+
+
+        // Get stuff from the other app first
+        private async void GetStuffFromContract()
+        {
+            await Task.Factory.StartNew(async () =>
+            {
+                // Get the properties of the shared package
+                this.sharedTitle = this.shareOperation.Data.Properties.Title;
+                this.sharedDescription = this.shareOperation.Data.Properties.Description;
+
+                // Now let's get the content! :)
+                // Text
+                if (this.shareOperation.Data.Contains(StandardDataFormats.Text))
+                {
+                    try
+                    {
+                        this.sharedText = await this.shareOperation.Data.GetTextAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("ERROR - ShareTargetHelper - GetText - " + ex);
+                    }
+                }
+
+                // Web-link
+                if (this.shareOperation.Data.Contains(StandardDataFormats.WebLink))
+                {
+                    try
+                    {
+                        this.sharedWebLink = await this.shareOperation.Data.GetWebLinkAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("ERROR - ShareTargetHelper - GetLink - " + ex);
+                    }
+                }
+            });
+        }
+
+        public void CloseShareTarget()
+        {
+            this.shareOperation.ReportCompleted();
+        }
+    }
+}
--- Unigram/Unigram/Views/ShareTargetPage.xaml.cs
+++ Unigram/Unigram/Views/ShareTargetPage.xaml.cs
@@ -16,8 +16,6 @@ using Unigram.ViewModels;
 using Unigram.Core.Dependency;
 using Windows.ApplicationModel.DataTransfer.ShareTarget;
 using System.Threading.Tasks;
-using Windows.ApplicationModel.DataTransfer;
-using System.Diagnostics;
 using Telegram.Api.TL;
 using Unigram.Core.Services;
 using Telegram.Api.Aggregator;
@@ -29,6 +27,7 @@ using System.Threading;
 using Telegram.Api.Services;
 using Telegram.Api.Helpers;
 using Windows.UI.Popups;
+using Unigram.Helpers;
 
 namespace Unigram.Views
 {
@@ -37,14 +36,7 @@ namespace Unigram.Views
         public MainViewModel ViewModel => DataContext as MainViewModel;
         private object _selectedDialog;
         private bool answerDirty = false;
-
-        // Create the ShareOperator and it's friends
-        ShareOperation shareOperation;
-        private string sharedTitle;
-        private string sharedDescription;
-        private string sharedText;
-        private Uri sharedWebLink;
-
+        private ShareTargetHelper sth;
 
         public ShareTargetPage()
         {
@@ -52,55 +44,20 @@ namespace Unigram.Views
             DataContext = UnigramContainer.Instance.ResolverType<MainViewModel>();
         }
 
-
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            this.shareOperation = (ShareOperation)e.Parameter;
-
-            await Task.Factory.StartNew(async () =>
-            {
-                // Get the properties of the shared package
-                this.sharedTitle = this.shareOperation.Data.Properties.Title;
-                this.sharedDescription = this.shareOperation.Data.Properties.Description;
-
-                // Now let's get the content! :)
-                // Text
-                if (this.shareOperation.Data.Contains(StandardDataFormats.Text))
-                {
-                    try
-                    {
-                        this.sharedText = await this.shareOperation.Data.GetTextAsync();
-                    }
-                    catch (Exception ex)
-                    {
-                        Debug.WriteLine("ERROR - ShareTargetHelper - GetText - " + ex);
-                    }
-                }
-
-                // Web-link
-                //if (this.shareOperation.Data.Contains(StandardDataFormats.WebLink))
-                //{
-                //    try
-                //    {
-                //        this.sharedWebLink = await this.shareOperation.Data.GetWebLinkAsync();
-                //    }
-                //    catch (Exception ex)
-                //    {
-                //        Debug.WriteLine("ERROR - ShareTargetHelper - GetLink - " + ex);
-                //    }
-                //}
-            });
+            sth = new ShareTargetHelper((ShareOperation)e.Parameter);
         }
-
-
 
         // Button
         private async void lvMasterChats_ItemClick(object sender, ItemClickEventArgs e)
         {
+            prgSendStatus.Value = 0;
             //TODO: Put the handling of the message in an background task to the user can quickly return to his/her work
             await this.showQuestionDialog();
             if (answerDirty == true)
             {
+                prgSendStatus.Value = 10;
                 // Set the selected Dialog with the clicked item
                 _selectedDialog = e.ClickedItem;
 
@@ -114,6 +71,7 @@ namespace Unigram.Views
                 var connectionService = new ConnectionService(deviceInfoService);
                 var manualResetEvent = new ManualResetEvent(false);
                 var protoService = new MTProtoService(deviceInfoService, updatesService, cacheService, transportService, connectionService);
+                prgSendStatus.Value = 20;
 
                 protoService.Initialized += (s, args) =>
                 {
@@ -122,32 +80,54 @@ namespace Unigram.Views
                     // First, get the dialogId to which we have to send the message
                     var toId = default(TLPeerBase);
                     toId = new TLPeerUser { Id = dialog.WithId };
-
+                    prgSendStatus.Value = 30;
 
                     // Now, prepare the message with the correct date and message itself.
                     var date = TLUtils.DateToUniversalTimeTLInt(protoService.ClientTicksDelta, DateTime.Now);
-                    var message = TLUtils.GetMessage(SettingsHelper.UserId, toId, TLMessageState.Sending, true, true, date, sharedText, new TLMessageMediaEmpty(), TLLong.Random(), replyToMsgId);
-                    var history = cacheService.GetHistory(SettingsHelper.UserId, toId, 1);
+                    prgSendStatus.Value = 40;
 
-                    // Now send the darn thing
-                    cacheService.SyncSendingMessage(message, null, toId, async (m) =>
+                    // Send the correct message according to the send content
+                    if (sth.sharedWebLink != null)
                     {
-                        await protoService.SendMessageAsync(message);
-                        manualResetEvent.Set();
-                    });
+                        // If the user shares a weblink then...
+                        var message = TLUtils.GetMessage(SettingsHelper.UserId, toId, TLMessageState.Sending, true, true, date, sth.sharedWebLink.AbsoluteUri, new TLMessageMediaEmpty(), TLLong.Random(), replyToMsgId);
+                        prgSendStatus.Value = 50;
+                        cacheService.SyncSendingMessage(message, null, toId, async (m) =>
+                        {
+                            await protoService.SendMessageAsync(message);
+                            manualResetEvent.Set();
+                            prgSendStatus.Value = 60;
+                        });
+                    }
+                    else
+                    {
+                        // In all other cases (mostly text only)
+                        var message = TLUtils.GetMessage(SettingsHelper.UserId, toId, TLMessageState.Sending, true, true, date, sth.sharedText, new TLMessageMediaEmpty(), TLLong.Random(), replyToMsgId);
+                        prgSendStatus.Value = 50;
+                        cacheService.SyncSendingMessage(message, null, toId, async (m) =>
+                        {
+                            await protoService.SendMessageAsync(message);
+                            manualResetEvent.Set();
+                            prgSendStatus.Value = 60;
+                        });
+                    }
+
+                    var history = cacheService.GetHistory(SettingsHelper.UserId, toId, 1);
+                    prgSendStatus.Value = 70;
                 };
                 protoService.InitializationFailed += (s, args) =>
                 {
                     manualResetEvent.Set();
                 };
                 cacheService.Initialize();
+                prgSendStatus.Value = 80;
                 protoService.Initialize();
+                prgSendStatus.Value = 90;
                 manualResetEvent.WaitOne(4000);
-
-                this.showErrorDialog();
+                prgSendStatus.Value = 100;
 
                 // Now close the shareoperation
-                //this.shareOperation.ReportCompleted();
+                sth.CloseShareTarget();
             }
 
         }
@@ -170,22 +150,6 @@ namespace Unigram.Views
             return answer;
         }
 
-        public async void showErrorDialog()
-        {
-            // Show Error dialog
-            MessageDialog errorDialog = new MessageDialog("Temp content");
-            errorDialog.Content = "The message has been send!";
-
-            errorDialog.Commands.Add(new UICommand("Close", new UICommandInvokedHandler(this.CommandInvokedHandlerClose)));
-            // Extra code to select the Close-option when an user presses on the Escape-button
-            errorDialog.CancelCommandIndex = 0;
-            // Show Dialog
-            await errorDialog.ShowAsync();
-        }
-        private void CommandInvokedHandlerClose(IUICommand command)
-        {
-            this.shareOperation.ReportCompleted();
-        }
         private void CommandInvokedHandlerAnswerYes(IUICommand command)
         {
             answerDirty = true;
