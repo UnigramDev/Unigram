5a7afd29d87baaf6815e0bd83d0c82e06d256585
Kesava Prasad Arul <kesavaprasadarul@gmail.com>
2016-07-01 19:21:20 +0530

    Added loading animations while Login/Signup Fixed Transitions in some pages Added SharedMedia mockup

--- Unigram/Unigram/Views/SharedMediaPage.xaml.cs
+++ Unigram/Unigram/Views/SharedMediaPage.xaml.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Unigram.Core.Dependency;
-using Unigram.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,20 +20,11 @@ namespace Unigram.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class DialogSharedMediaPage : Page
+    public sealed partial class SharedMediaPage : Page
     {
-        public DialogSharedMediaViewModel ViewModel => DataContext as DialogSharedMediaViewModel;
-
-        public DialogSharedMediaPage()
+        public SharedMediaPage()
         {
-            InitializeComponent();
-            this.SizeChanged += DialogSharedMediaPage_SizeChanged;
-            DataContext = UnigramContainer.Instance.ResolverType<DialogSharedMediaViewModel>();
+            this.InitializeComponent();
         }
-
-        private void DialogSharedMediaPage_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            this.UpdateLayout();
-         }
     }
 }
