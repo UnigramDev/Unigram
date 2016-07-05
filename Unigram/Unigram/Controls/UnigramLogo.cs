edf48b169e372fe5f5dd9ebe2a64ca6683b3909e
Fela Ameghino <frayx@live.it>
2016-07-05 16:16:13 +0200

    Got a "Just do it"

---
+++ Unigram/Unigram/Controls/UnigramLogo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Unigram.Controls
+{
+    public class UnigramLogo : Control
+    {
+        public UnigramLogo()
+        {
+            DefaultStyleKey = typeof(UnigramLogo);
+        }
+    }
+}
