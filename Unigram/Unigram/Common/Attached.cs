f07f5c1d8b0e610970cacfc4dcca9c9b26335408
Fela Ameghino <frayx@live.it>
2016-09-16 15:52:05 +0200

    Stickers! Received and sent

---
+++ Unigram/Unigram/Common/Attached.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace Unigram.Common
+{
+    public class Attached
+    {
+        #region Tapped
+        public static ICommand GetTapped(DependencyObject obj)
+        {
+            return (ICommand)obj.GetValue(TappedProperty);
+        }
+
+        public static void SetTapped(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(TappedProperty, value);
+        }
+
+        public static readonly DependencyProperty TappedProperty =
+            DependencyProperty.RegisterAttached("Tapped", typeof(ICommand), typeof(Attached), new PropertyMetadata(null, OnTappedChanged));
+
+        private static void OnTappedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var sender = d as Control;
+            if (sender != null)
+            {
+                sender.IsTapEnabled = true;
+                sender.Tapped -= OnTapped;
+                sender.Tapped += OnTapped;
+            }
+        }
+
+        private static void OnTapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            var command = GetTapped(sender as Control);
+            var parameter = GetTappedParameter(sender as Control);
+            if (command != null)
+            {
+                command.Execute(parameter ?? (sender as Control).DataContext);
+            }
+        }
+        #endregion
+
+        #region TappedParameter
+        public static object GetTappedParameter(DependencyObject obj)
+        {
+            return (object)obj.GetValue(TappedParameterProperty);
+        }
+
+        public static void SetTappedParameter(DependencyObject obj, object value)
+        {
+            obj.SetValue(TappedParameterProperty, value);
+        }
+
+        public static readonly DependencyProperty TappedParameterProperty =
+            DependencyProperty.RegisterAttached("TappedParameter", typeof(object), typeof(Attached), new PropertyMetadata(null));
+        #endregion
+    }
+}
---
+++ Unigram/Unigram/Converters/TLIntToDateTimeConverter.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Globalization;
+using Telegram.Api.Helpers;
+using Telegram.Api.Services;
+using Telegram.Api.TL;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Unigram.Converters
+{
+    public class TLIntToDateTimeConverter : DependencyObject, IValueConverter
+    {
+        #region TodayFormat
+        public static readonly DependencyProperty TodayFormatProperty =
+            DependencyProperty.Register("TodayFormat", 
+                                        typeof (string), typeof (TLIntToDateTimeConverter),
+                                        new PropertyMetadata(default(string)));
+
+        public string TodayFormat
+        {
+            get { return (string) GetValue(TodayFormatProperty); }
+            set { SetValue(TodayFormatProperty, value); }
+        }
+        #endregion
+
+        #region YesterdayString
+        public static readonly DependencyProperty YesterdayStringProperty =
+            DependencyProperty.Register("YesterdayString", typeof (string), typeof (TLIntToDateTimeConverter), new PropertyMetadata(default(string)));
+
+        public string YesterdayString
+        {
+            get { return (string) GetValue(YesterdayStringProperty); }
+            set { SetValue(YesterdayStringProperty, value); }
+        }
+        #endregion
+
+        #region YesterdayFormat
+        public static readonly DependencyProperty YesterdayFormatProperty =
+            DependencyProperty.Register("YesterdayFormat", 
+                                        typeof (string), typeof (TLIntToDateTimeConverter),
+                                        new PropertyMetadata(default(string)));
+
+        public string YesterdayFormat
+        {
+            get { return (string) GetValue(YesterdayFormatProperty); }
+            set { SetValue(YesterdayFormatProperty, value); }
+        }
+        #endregion
+
+        #region WeekFormat
+        public string WeekFormat
+        {
+            get { return (string)GetValue(WeekFormatProperty); }
+            set { SetValue(WeekFormatProperty, value); }
+        }
+
+        public static readonly DependencyProperty WeekFormatProperty =
+            DependencyProperty.Register("WeekFormat", typeof(string), typeof(TLIntToDateTimeConverter), new PropertyMetadata(default(string)));
+        #endregion
+
+        #region RegularFormat
+        public static readonly DependencyProperty RegularFormatProperty =
+            DependencyProperty.Register("RegularFormat", 
+                                        typeof (string), typeof (TLIntToDateTimeConverter),
+                                        new PropertyMetadata(default(string)));
+
+        public string RegularFormat
+        {
+            get { return (string) GetValue(RegularFormatProperty); }
+            set { SetValue(RegularFormatProperty, value); }
+        }
+        #endregion
+
+        public static readonly DependencyProperty LongRegularFormatProperty =
+            DependencyProperty.Register("LongRegularFormat", 
+                                        typeof (string), typeof (TLIntToDateTimeConverter), 
+                                        new PropertyMetadata(default(string)));
+
+        public string LongRegularFormat
+        {
+            get { return (string) GetValue(LongRegularFormatProperty); }
+            set { SetValue(LongRegularFormatProperty, value); }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var clientDelta = MTProtoService.Current.ClientTicksDelta;
+            var utc0SecsLong = (int)value * 4294967296 - clientDelta;
+            var utc0SecsInt = utc0SecsLong / 4294967296.0;
+            var dateTime = Telegram.Api.Helpers.Utils.UnixTimestampToDateTime(utc0SecsInt);
+
+            var cultureInfo = (CultureInfo)CultureInfo.CurrentUICulture.Clone();
+            var shortTimePattern = Utils.GetShortTimePattern(ref cultureInfo);
+
+            if (WeekFormat == null)
+                return dateTime.ToString(string.Format(TodayFormat, shortTimePattern), cultureInfo);
+
+            //Today
+            if ((dateTime.Date == DateTime.Now.Date) && !string.IsNullOrEmpty(TodayFormat))
+                return dateTime.ToString(string.Format(TodayFormat, shortTimePattern), cultureInfo);
+
+            //Yesterday
+            if ((dateTime.Date.AddDays(1) == DateTime.Now.Date) && !string.IsNullOrEmpty(YesterdayString))
+                return YesterdayString;
+
+            if ((dateTime.Date.AddDays(1) == DateTime.Now.Date) && !string.IsNullOrEmpty(YesterdayFormat))
+                return dateTime.ToString(string.Format(YesterdayFormat, shortTimePattern), cultureInfo);
+
+            if (dateTime.Date.AddDays(7) >= DateTime.Now.Date && !string.IsNullOrEmpty(WeekFormat))
+                return dateTime.ToString(string.Format(WeekFormat, shortTimePattern), cultureInfo);
+
+            //Long time ago (no more than one year ago)
+            if (dateTime.Date.AddDays(365) >= DateTime.Now.Date && !string.IsNullOrEmpty(RegularFormat))
+                return dateTime.ToString(string.Format(RegularFormat, shortTimePattern), cultureInfo);
+
+            //Long long time ago
+            return dateTime.ToString(string.Format(LongRegularFormat, shortTimePattern), cultureInfo);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
