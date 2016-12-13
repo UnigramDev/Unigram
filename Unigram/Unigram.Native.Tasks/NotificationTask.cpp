595bb6bbf7bc8fbee23aa0e3f2bbf1f8703e6256
Fela Ameghino <frayx@live.it>
2016-12-13 15:28:10 +0100

    Moving NotificationTask to C++

---
+++ Unigram/Unigram.Native.Tasks/NotificationTask.cpp
@@ -0,0 +1,11 @@
+﻿#include "pch.h"
+#include "NotificationTask.h"
+
+using namespace Windows::Data::Json;
+using namespace Unigram::Native::Tasks;
+using namespace Platform;
+
+void NotificationTask::Run(IBackgroundTaskInstance^ taskInstance)
+{
+	auto deferral = taskInstance->GetDeferral();
+}
---
+++ Unigram/Unigram.Native.Tasks/NotificationTask.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+using namespace Windows::ApplicationModel::Background;
+using namespace Windows::Networking::PushNotifications;
+
+
+namespace Unigram
+{
+	namespace Native 
+	{
+		namespace Tasks
+		{
+			public ref class NotificationTask sealed : public IBackgroundTask
+			{
+			public:
+				virtual void Run(IBackgroundTaskInstance^ taskInstance);
+			};
+		}
+	}
+}
