52bb42a1fa11f9e272de84e1614c8d6f08f0ef8f
Fela Ameghino <frayx@live.it>
2017-01-09 22:46:00 +0100

    - Pin and unpin chats - Pin and unpin messages - Messages are now editable - You can now use your drafted messages - Better dark theme handling - Many UI fixes and improvements - Stickers updated - Chats are now marked as read when opened

--- Unigram/Unigram/ViewModels/DialogViewModel.cs
+++ Unigram/Unigram/ViewModels/DialogViewModel.cs
@@ -424,24 +424,24 @@ namespace Unigram.ViewModels
                 }
             }
 
-            //if (dialog != null && Messages.Count > 0)
-            //{
-            //    var unread = dialog.UnreadCount;
-            //    if (Peer is TLInputPeerChannel)
-            //    {
-            //        if (channel != null)
-            //        {
-            //            await ProtoService.ReadHistoryAsync(channel, dialog.TopMessage);
-            //        }
-            //    }
-            //    else
-            //    {
-            //        await ProtoService.ReadHistoryAsync(Peer, dialog.TopMessage, 0);
-            //    }
+            if (dialog != null && Messages.Count > 0)
+            {
+                var unread = dialog.UnreadCount;
+                if (Peer is TLInputPeerChannel)
+                {
+                    if (channel != null)
+                    {
+                        await ProtoService.ReadHistoryAsync(channel, dialog.TopMessage);
+                    }
+                }
+                else
+                {
+                    await ProtoService.ReadHistoryAsync(Peer, dialog.TopMessage, 0);
+                }
 
-            //    dialog.UnreadCount = dialog.UnreadCount - unread;
-            //    dialog.RaisePropertyChanged(() => dialog.UnreadCount);
-            //}
+                dialog.UnreadCount = dialog.UnreadCount - unread;
+                dialog.RaisePropertyChanged(() => dialog.UnreadCount);
+            }
 
             Aggregator.Subscribe(this);
 
