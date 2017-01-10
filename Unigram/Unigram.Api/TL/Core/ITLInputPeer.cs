6507a3b79fae33e837d63ce29e59229310a2a57b
Fela Ameghino <frayx@live.it>
2017-01-10 12:59:58 +0100

    Added ITLInputPeer

---
+++ Unigram/Unigram.Api/TL/Core/ITLInputPeer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Api.TL;
+
+namespace Telegram.Api.TL
+{
+    public interface ITLInputPeer
+    {
+        TLInputPeerBase ToInputPeer();
+    }
+}
