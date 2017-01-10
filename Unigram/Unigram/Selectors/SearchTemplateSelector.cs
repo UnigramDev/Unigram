a9234b2c3fedd3bc37c6ffe1347f0c7138fe37d7
Fela Ameghino <frayx@live.it>
2017-01-10 19:04:45 +0100

    Global search

---
+++ Unigram/Unigram/Selectors/SearchTemplateSelector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Api.TL;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Unigram.Selectors
+{
+    public class SearchTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate UserTemplate { get; set; }
+
+        public DataTemplate ChannelTemplate { get; set; }
+
+        public DataTemplate MessageTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            var user = item as TLUser;
+            if (user != null)
+            {
+                return UserTemplate;
+            }
+
+            var channel = item as TLChannel;
+            if (channel != null)
+            {
+                return ChannelTemplate;
+            }
+
+            var dialog = item as TLDialog;
+            if (dialog != null)
+            {
+                return MessageTemplate;
+            }
+
+            return base.SelectTemplateCore(item, container);
+        }
+    }
+}
