ab358f6a796859bd881c4dba78514bea0d2980cb
Fela Ameghino <frayx@live.it>
2017-02-21 09:54:28 +0100

    Shared contacts are now showing contact name instead of user name

---
+++ Unigram/Unigram.Api/TL/Partial/TLMessageMediaContact.Partial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram.Api.TL
+{
+    public partial class TLMessageMediaContact
+    {
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", FirstName, LastName).Trim();
+            }
+        }
+    }
+}
