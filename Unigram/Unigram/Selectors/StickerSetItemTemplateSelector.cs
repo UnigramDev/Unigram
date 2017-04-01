0d1c15592a756deeceacc293c64e438156a1cf4c
Fela Ameghino <frayx@live.it>
2017-04-01 20:36:21 +0200

    Hidden emoji for masks

---
+++ Unigram/Unigram/Selectors/StickerSetItemTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Unigram.Selectors
+{
+    public class StickerSetItemTemplateSelector : DataTemplateSelector
+    {
+        public bool IsMasks { get; set; }
+
+        public DataTemplate ImageTemplate { get; set; }
+        public DataTemplate MaskTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            return IsMasks ? MaskTemplate : ImageTemplate;
+        }
+    }
+}
