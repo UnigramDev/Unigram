2b2c6df444a3ca8ab4e8f7672f2f71648fbeb6d3
r2d2rigo <r2d2rigo@gmail.com>
2017-04-20 23:36:15 +0100

    ShareTargetPage now uses SimpleDialogListViewItem again instead of DialogListViewItem.

--- Unigram/Unigram/Controls/Items/SimpleDialogListViewItem.xaml.cs
+++ Unigram/Unigram/Controls/Items/SimpleDialogListViewItem.xaml.cs
@@ -25,10 +25,19 @@ namespace Unigram.Controls.Items
     {
         public TLDialog ViewModel => DataContext as TLDialog;
         private TLDialog _oldViewModel;
+        private TLDialog _oldValue;
 
         public SimpleDialogListViewItem()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            DataContextChanged += (s, args) =>
+            {
+                if (ViewModel != null && ViewModel != _oldValue) Bindings.Update();
+                if (ViewModel == null) Bindings.StopTracking();
+
+                _oldValue = ViewModel;
+            };
 
             DataContextChanged += OnDataContextChanged;
         }
@@ -45,8 +54,6 @@ namespace Unigram.Controls.Items
             {
                 _oldViewModel = ViewModel;
                 ViewModel.PropertyChanged += OnPropertyChanged;
-
-                UpdatePicture();
             }
         }
 
@@ -54,91 +61,9 @@ namespace Unigram.Controls.Items
         {
             if (e.PropertyName == "Self")
             {
-
-            }
-            else if (e.PropertyName == "TopMessageItem")
-            {
-
-            }
-            else if (e.PropertyName == "UnreadCount")
-            {
-
             }
             else if (e.PropertyName == "With")
             {
-                UpdatePicture();
-            }
-        }
-
-        private void UpdatePicture()
-        {
-            switch (GetColorIndex(ViewModel.WithId))
-            {
-                case 0:
-                    Placeholder.Fill = Application.Current.Resources["Placeholder0Brush"] as SolidColorBrush;
-                    break;
-                case 1:
-                    Placeholder.Fill = Application.Current.Resources["Placeholder1Brush"] as SolidColorBrush;
-                    break;
-                case 2:
-                    Placeholder.Fill = Application.Current.Resources["Placeholder2Brush"] as SolidColorBrush;
-                    break;
-                case 3:
-                    Placeholder.Fill = Application.Current.Resources["Placeholder3Brush"] as SolidColorBrush;
-                    break;
-                case 4:
-                    Placeholder.Fill = Application.Current.Resources["Placeholder4Brush"] as SolidColorBrush;
-                    break;
-                case 5:
-                    Placeholder.Fill = Application.Current.Resources["Placeholder5Brush"] as SolidColorBrush;
-                    break;
-                default:
-                    Placeholder.Fill = Application.Current.Resources["PlaceholderListViewItemPlaceholderBackgroundThemeBrush"] as SolidColorBrush;
-                    break;
-            }
-        }
-
-        private int GetColorIndex(int id)
-        {
-            if (id < 0)
-            {
-                id += 256;
-            }
-
-            try
-            {
-                var str = string.Format("{0}{1}", id, MTProtoService.Current.CurrentUserId);
-                if (str.Length > 15)
-                {
-                    str = str.Substring(0, 15);
-                }
-
-                var input = CryptographicBuffer.ConvertStringToBinary(str, BinaryStringEncoding.Utf8);
-                var hasher = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5);
-                var hashed = hasher.HashData(input);
-                CryptographicBuffer.CopyToByteArray(hashed, out byte[] digest);
-
-                var boh = ((id & 0x300000000) == 0x300000000);
-
-                return digest[id % 0x0F] & ((ViewModel.With is TLPeerUser) ? 0x07 : 0x03);
-            }
-            catch { }
-
-            return id % 6;
-        }
-
-        private void UpdateTimeLabel()
-        {
-            var topMessage = ViewModel?.TopMessageItem as TLMessageBase;
-            if (topMessage != null)
-            {
-                var clientDelta = MTProtoService.Current.ClientTicksDelta;
-                var utc0SecsLong = topMessage.Date * 4294967296 - clientDelta;
-                var utc0SecsInt = utc0SecsLong / 4294967296.0;
-                var dateTime = Utils.UnixTimestampToDateTime(utc0SecsInt);
-
-                var cultureInfo = (CultureInfo)CultureInfo.CurrentUICulture.Clone();
-                var shortTimePattern = Utils.GetShortTimePattern(ref cultureInfo);         
             }
         }
 
