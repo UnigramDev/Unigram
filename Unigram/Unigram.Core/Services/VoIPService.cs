d9d0f63ca76201738126f7b3fa75b3b696e5defa
Fela Ameghino <frayx@live.it>
2017-04-21 17:30:40 +0200

    VoIP refactoring

---
+++ Unigram/Unigram.Core/Services/VoIPService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Api.Aggregator;
+using Telegram.Api.TL;
+
+namespace Unigram.Core.Services
+{
+    public interface IVoIPService
+    {
+
+    }
+
+    public class VoIPService : IVoIPService, IHandle<TLUpdatePhoneCall>, IHandle
+    {
+        public void Handle(TLUpdatePhoneCall update)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
