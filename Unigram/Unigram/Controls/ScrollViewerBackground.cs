6f45f568fd2dd259fbcdeaeefb757ecb0ec94262
Fela Ameghino <frayx@live.it>
2017-04-21 20:22:28 +0200

    Feedback button in ArticlePage is now working

--- Unigram/Unigram/Controls/ScrollViewerBackground.cs
+++ Unigram/Unigram/Controls/ScrollViewerBackground.cs
@@ -15,13 +15,16 @@ namespace Unigram.Controls
 {
     public class ScrollViewerBackground : Control
     {
+        private ScrollViewer _scrollViewer;
         private Compositor _compositor;
         private SpriteVisual _visual;
+        private Visual _rootVisual;
         private ExpressionAnimation _animationSize;
         private ExpressionAnimation _animationOffset;
 
         public ScrollViewerBackground()
         {
+            //DefaultStyleKey = typeof(ScrollViewerBackground);
             _compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
             _visual = _compositor.CreateSpriteVisual();
 
@@ -52,6 +55,14 @@ namespace Unigram.Controls
                 return;
             }
 
+            var root = scrollViewer.Content as FrameworkElement;
+            if (root == null && VerticalAlignment == VerticalAlignment.Bottom)
+            {
+                return;
+            }
+
+            _scrollViewer = scrollViewer;
+
             if (VerticalAlignment == VerticalAlignment.Top)
             {
                 _animationSize = _compositor.CreateExpressionAnimation("Max(Scroll.Translation.Y + 1, 0)");
@@ -61,8 +72,29 @@ namespace Unigram.Controls
             }
             else if (VerticalAlignment == VerticalAlignment.Bottom)
             {
+                _rootVisual = ElementCompositionPreview.GetElementVisual(root);
+                _rootVisual.Properties.InsertVector2("Test", new Vector2((float)_scrollViewer.ScrollableWidth, (float)_scrollViewer.ScrollableHeight));
 
+                root.SizeChanged -= OnViewportChanged;
+                root.SizeChanged += OnViewportChanged;
+
+                _animationSize = _compositor.CreateExpressionAnimation("-Scroll.Translation.Y - Root.Test.Y");
+                _animationSize.SetReferenceParameter("Scroll", props);
+                _animationSize.SetReferenceParameter("Root", _rootVisual);
+
+                _animationOffset = _compositor.CreateExpressionAnimation("-Scroll.Translation.Y - Root.Size.Y");
+                _animationOffset.SetReferenceParameter("Scroll", props);
+                _animationOffset.SetReferenceParameter("Root", _rootVisual);
+
+                //_visual.AnchorPoint = new Vector2(0, 1);
+                _visual.StartAnimation("Size.Y", _animationSize);
+                _visual.StartAnimation("Offset.Y", _animationOffset);
             }
+        }
+
+        private void OnViewportChanged(object sender, SizeChangedEventArgs e)
+        {
+            _rootVisual.Properties.InsertVector2("Test", new Vector2((float)_scrollViewer.ScrollableWidth, (float)_scrollViewer.ScrollableHeight));
         }
 
         private void OnSizeChanged(object sender, SizeChangedEventArgs e)
@@ -70,10 +102,16 @@ namespace Unigram.Controls
             _visual.Size = new Vector2((float)e.NewSize.Width, _visual.Size.Y);
 
             if (_animationSize != null)
+            {
+                _visual.StopAnimation("Size.Y");
                 _visual.StartAnimation("Size.Y", _animationSize);
+            }
 
             if (_animationOffset != null)
-                _visual.StartAnimation("Offset.Y", _animationSize);
+            {
+                _visual.StopAnimation("Offset.Y");
+                _visual.StartAnimation("Offset.Y", _animationOffset);
+            }
         }
 
         private void OnBackgroundChanged(DependencyObject sender, DependencyProperty dp)
