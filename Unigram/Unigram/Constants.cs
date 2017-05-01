cd2268d6f9c67675ed5528cc1ba9fbacaae7b464
Fela Ameghino <frayx@live.it>
2017-05-01 11:42:39 +0200

    telegram.dog too

--- Unigram/Unigram/Constants.cs
+++ Unigram/Unigram/Constants.cs
@@ -14,6 +14,7 @@ namespace Unigram
         public static readonly string[] TelegramHosts = new string[]
         {
             "telegram.me",
+            "telegram.dog",
             "t.me",
             "telesco.pe"
         };
