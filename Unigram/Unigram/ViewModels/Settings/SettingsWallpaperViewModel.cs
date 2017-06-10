c776dd46126cf90aaf86284bdfc74f8e95cd09d4
Fela Ameghino <frayx@live.it>
2017-06-10 11:33:58 +0200

    Working on wallpapers

---
+++ Unigram/Unigram/ViewModels/Settings/SettingsWallpaperViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Api.Aggregator;
+using Telegram.Api.Services;
+using Telegram.Api.Services.Cache;
+using Telegram.Api.TL;
+using Unigram.Core.Common;
+
+namespace Unigram.ViewModels.Settings
+{
+    public class SettingsWallPaperViewModel : UnigramViewModelBase
+    {
+        public SettingsWallPaperViewModel(IMTProtoService protoService, ICacheService cacheService, ITelegramEventAggregator aggregator)
+            : base(protoService, cacheService, aggregator)
+        {
+            Items = new MvxObservableCollection<TLWallPaperBase>();
+            ProtoService.GetWallpapersAsync(result =>
+            {
+                Items.ReplaceWith(result);
+            });
+        }
+
+        public MvxObservableCollection<TLWallPaperBase> Items { get; private set; }
+    }
+}
