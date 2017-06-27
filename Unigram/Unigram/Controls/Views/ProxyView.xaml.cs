ca10f5f20558ea5eb593d04686a80ba794446716
Fela Ameghino <frayx@live.it>
2017-06-27 18:14:15 +0200

    Ability to share proxy links

--- Unigram/Unigram/Controls/Views/ProxyView.xaml.cs
+++ Unigram/Unigram/Controls/Views/ProxyView.xaml.cs
@@ -4,9 +4,15 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Telegram.Api.Services.Cache;
 using Unigram.Common;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,6 +32,97 @@ namespace Unigram.Controls.Views
             this.InitializeComponent();
         }
 
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (ApiInformation.IsEventPresent("Windows.ApplicationModel.DataTransfer.DataTransferManager", "ShareProvidersRequested"))
+            {
+                DataTransferManager.GetForCurrentView().ShareProvidersRequested -= OnShareProvidersRequested;
+                DataTransferManager.GetForCurrentView().ShareProvidersRequested += OnShareProvidersRequested;
+            }
+
+            DataTransferManager.GetForCurrentView().DataRequested -= OnDataRequested;
+            DataTransferManager.GetForCurrentView().DataRequested += OnDataRequested;
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            if (ApiInformation.IsEventPresent("Windows.ApplicationModel.DataTransfer.DataTransferManager", "ShareProvidersRequested"))
+            {
+                DataTransferManager.GetForCurrentView().ShareProvidersRequested -= OnShareProvidersRequested;
+            }
+
+            DataTransferManager.GetForCurrentView().DataRequested -= OnDataRequested;
+        }
+
+        private void OnShareProvidersRequested(DataTransferManager sender, ShareProvidersRequestedEventArgs args)
+        {
+            if (args.Data.Contains(StandardDataFormats.WebLink))
+            {
+                var icon = RandomAccessStreamReference.CreateFromUri(new Uri(@"ms-appx:///Assets/Images/ShareProvider_CopyLink24x24.png"));
+                var provider = new ShareProvider("Copy link", icon, (Color)App.Current.Resources["SystemAccentColor"], OnShareToClipboard);
+                args.Providers.Add(provider);
+            }
+        }
+
+        private async void OnShareToClipboard(ShareProviderOperation operation)
+        {
+            var webLink = await operation.Data.GetWebLinkAsync();
+            var package = new DataPackage();
+            package.SetText(webLink.ToString());
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                Clipboard.SetContent(package);
+                operation.ReportCompleted();
+            });
+        }
+
+        private void OnDataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var config = InMemoryCacheService.Current.GetConfig();
+            if (config == null)
+            {
+                return;
+            }
+
+            var linkPrefix = config.MeUrlPrefix;
+            if (linkPrefix.EndsWith("/"))
+            {
+                linkPrefix = linkPrefix.Substring(0, linkPrefix.Length - 1);
+            }
+            if (linkPrefix.StartsWith("https://"))
+            {
+                linkPrefix = linkPrefix.Substring(8);
+            }
+            else if (linkPrefix.StartsWith("http://"))
+            {
+                linkPrefix = linkPrefix.Substring(7);
+            }
+
+            var builder = new List<string>();
+            if (Server != null)
+            {
+                builder.Add("server=" + Server);
+            }
+            if (Port != null)
+            {
+                builder.Add("port=" + Port);
+            }
+            if (Username != null)
+            {
+                builder.Add("user=" + Username);
+            }
+            if (Password != null)
+            {
+                builder.Add("pass=" + Password);
+            }
+
+            var package = args.Request.Data;
+            package.Properties.Title = "Proxy Settings";
+            package.SetText($"https://{linkPrefix}/socks?{string.Join("&", builder)}");
+            package.SetWebLink(new Uri($"https://{linkPrefix}/socks?{string.Join("&", builder)}"));
+        }
+
         public bool IsProxyEnabled
         {
             get
@@ -42,7 +139,7 @@ namespace Unigram.Controls.Views
         {
             get
             {
-                return FieldServer.Text;
+                return string.IsNullOrWhiteSpace(FieldServer.Text) ? null : FieldServer.Text;
             }
             set
             {
@@ -54,7 +151,7 @@ namespace Unigram.Controls.Views
         {
             get
             {
-                return FieldPort.Text;
+                return string.IsNullOrWhiteSpace(FieldPort.Text) ? null : FieldPort.Text;
             }
             set
             {
@@ -66,7 +163,7 @@ namespace Unigram.Controls.Views
         {
             get
             {
-                return FieldUsername.Text;
+                return string.IsNullOrWhiteSpace(FieldUsername.Text) ? null : FieldUsername.Text;
             }
             set
             {
@@ -78,7 +175,7 @@ namespace Unigram.Controls.Views
         {
             get
             {
-                return FieldPassword.Password;
+                return string.IsNullOrWhiteSpace(FieldPassword.Password) ? null : FieldPassword.Password;
             }
             set
             {
@@ -105,6 +202,11 @@ namespace Unigram.Controls.Views
 
         private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
+        }
+
+        private void Share_Click(object sender, RoutedEventArgs e)
+        {
+            DataTransferManager.ShowShareUI();
         }
     }
 }
