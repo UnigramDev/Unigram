8a993076aa78fb409e18e79eb3e815ff9174bcfe
Fela Ameghino <frayx@live.it>
2017-07-27 20:18:58 +0200

    Fixed keyboard hiding gifs preview

--- Unigram/Unigram/Controls/ZoomableGridViewPopup.xaml.cs
+++ Unigram/Unigram/Controls/ZoomableGridViewPopup.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,6 +23,30 @@ namespace Unigram.Controls
         public ZoomableGridViewPopup()
         {
             this.InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Margin = new Thickness(0, 0, 0, InputPane.GetForCurrentView().OccludedRect.Bottom);
+
+            InputPane.GetForCurrentView().Showing += InputPane_Showing;
+            InputPane.GetForCurrentView().Hiding += InputPane_Hiding;
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            InputPane.GetForCurrentView().Showing -= InputPane_Showing;
+            InputPane.GetForCurrentView().Hiding -= InputPane_Hiding;
+        }
+
+        private void InputPane_Showing(InputPane sender, InputPaneVisibilityEventArgs args)
+        {
+            Margin = new Thickness(0, 0, 0, args.OccludedRect.Bottom);
+        }
+
+        private void InputPane_Hiding(InputPane sender, InputPaneVisibilityEventArgs args)
+        {
+            Margin = new Thickness();
         }
     }
 }
