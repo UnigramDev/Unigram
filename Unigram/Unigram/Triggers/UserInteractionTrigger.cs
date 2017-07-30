7cb851e2934a01674e80f8921a6392e0090f1291
Fela Ameghino <frayx@live.it>
2017-07-30 16:56:18 +0200

    Expand stickers panel

---
+++ Unigram/Unigram/Triggers/UserInteractionTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+namespace Unigram.Triggers
+{
+    public class UserInteractionTrigger : StateTriggerBase
+    {
+        public UserInteractionTrigger()
+        {
+            Window.Current.SizeChanged += SizeChanged;
+        }
+
+        private UserInteractionMode _mode;
+        public UserInteractionMode Mode
+        {
+            get
+            {
+                return _mode;
+            }
+            set
+            {
+                _mode = value;
+            }
+        }
+
+        private async void SizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                SetActive(UIViewSettings.GetForCurrentView().UserInteractionMode == _mode);
+            });
+        }
+    }
+}
