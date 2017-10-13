bcccabab15696c0a2401fa2cae8b51bf517b9d5e
Callum Moffat <13494563+callummoffat@users.noreply.github.com>
2017-10-14 12:39:14 +1300

    Begun making SettingsPage localization-ready

--- Unigram/Unigram/Strings/en/Resources.cs
+++ Unigram/Unigram/Strings/en/Resources.cs
@@ -17,7 +17,7 @@
 // 	This code contains a helper class exposing property representations
 // 	of the string resources defined in the specified .ResW file
 // 
-// 	Generated: 10/13/2017 14:27:28
+// 	Generated: 10/13/2017 17:12:29
 // </auto-generatedInfo>
 // --------------------------------------------------------------------------------------------------
 namespace Unigram.Strings
