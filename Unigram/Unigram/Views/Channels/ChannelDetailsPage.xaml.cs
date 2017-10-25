92fe5a1ac7c2cb18056b1ef14b5ac953555bc6f7
Luca Montanari <luca.montanari@manageddesigns.it>
2017-10-25 16:28:59 +0200

    Fixed ArgumentException when trying to show a context menu on first pixel (X or Y axis) of selected element/area

--- Unigram/Unigram/Views/Channels/ChannelDetailsPage.xaml.cs
+++ Unigram/Unigram/Views/Channels/ChannelDetailsPage.xaml.cs
@@ -108,6 +108,11 @@ namespace Unigram.Views.Channels
 
             if (menu.Items.Count > 0 && args.TryGetPosition(sender, out Point point))
             {
+                if (point.X < 0 || point.Y < 0)
+                {
+                    point = new Point(Math.Max(point.X, 0), Math.Max(point.Y, 0));
+                }
+
                 menu.ShowAt(sender, point);
             }
         }
