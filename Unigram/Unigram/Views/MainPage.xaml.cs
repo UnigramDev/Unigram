76c8f93de3c451923b5a726f50bcab6f90608d98
Fela Ameghino <frayx@live.it>
2017-10-29 00:44:57 +0200

    Moved saved messages entry to a more visible location

--- Unigram/Unigram/Views/MainPage.xaml.cs
+++ Unigram/Unigram/Views/MainPage.xaml.cs
@@ -873,6 +873,10 @@ namespace Unigram.Views
             {
                 rpMasterTitlebar.SelectedIndex = 3;
             }
+            else if (args.ClickedItem == NavigationSavedMessages && ViewModel.Contacts.Self != null)
+            {
+                MasterDetail.NavigationService.NavigateToDialog(ViewModel.Contacts.Self);
+            }
             else if (args.ClickedItem == NavigationOfficialChannel)
             {
                 MessageHelper.NavigateToUsername(ViewModel.ProtoService, "unigram", null, null, null);
