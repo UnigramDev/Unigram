a95505cdc1ed7f437f972e6c4babb64c6f3ea845
Fela Ameghino <frayx@live.it>
2017-11-03 21:17:48 +0100

    Fixed broken multi pts calculation

--- Unigram/Unigram.Api/TL/Partial/TLUpdateBase.Partial.cs
+++ Unigram/Unigram.Api/TL/Partial/TLUpdateBase.Partial.cs
@@ -8,13 +8,15 @@ namespace Telegram.Api.TL
 {
     public abstract partial class TLUpdateBase
     {
-        public virtual IList<int> GetPts()
-        {
-            return new List<int>();
-        }
+        //public virtual IList<int> GetPts()
+        //{
+        //    return new int[0];
+        //}
+
+        public abstract IList<int> GetPts();
     }
 
-    public partial class TLUpdateNewMessage : ITLMultiPts
+    public partial class TLUpdateNewMessage : TLUpdateBase, ITLMultiPts
     {
         public override IList<int> GetPts()
         {
@@ -22,55 +24,52 @@ namespace Telegram.Api.TL
         }
     }
 
-    public partial class TLUpdateChatParticipantAdd
+    public partial class TLUpdateChatParticipantAdd : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateChatParticipantDelete
+    public partial class TLUpdateChatParticipantDelete : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateNewEncryptedMessage
+    public partial class TLUpdateNewEncryptedMessage : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateEncryption
+    public partial class TLUpdateEncryption : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateMessageID
+    public partial class TLUpdateMessageID : TLUpdateBase
     {
+        public override string ToString()
+        {
+            return string.Format("TLUpdateMessageId id={0} random_id={1}", Id, RandomId);
+        }
+
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    //public partial class TLUpdateReadMessages
-    //{
-    //    public override IList<int> GetPts()
-    //    {
-    //        return TLUtils.GetPtsRange(Pts, PtsCount);
-    //    }
-    //}
-
-    public partial class TLUpdateReadMessagesContents : ITLMultiPts
+    public partial class TLUpdateReadMessagesContents : TLUpdateBase, ITLMultiPts
     {
         public override IList<int> GetPts()
         {
@@ -78,206 +77,46 @@ namespace Telegram.Api.TL
         }
     }
 
-    public partial class TLUpdateReadHistoryInbox : ITLMultiPts
+    public partial class TLUpdateReadHistoryInbox : TLUpdateBase, ITLMultiPts
     {
-        public override IList<int> GetPts()
-        {
-            return TLUtils.GetPtsRange(Pts, PtsCount);
-        }
-
         public override string ToString()
         {
             return string.Format("TLUpdateReadHistoryInbox peer={0} max_id={1} pts={2} pts_count={3}", Peer, MaxId, Pts, PtsCount);
         }
-    }
 
-    public partial class TLUpdateReadHistoryOutbox : ITLMultiPts
-    {
         public override IList<int> GetPts()
         {
             return TLUtils.GetPtsRange(Pts, PtsCount);
         }
+    }
 
+    public partial class TLUpdateReadHistoryOutbox : TLUpdateBase, ITLMultiPts
+    {
         public override string ToString()
         {
             return string.Format("TLUpdateReadHistoryOutbox peer={0} max_id={1} pts={2} pts_count={3}", Peer, MaxId, Pts, PtsCount);
         }
-    }
 
-    public partial class TLUpdateEncryptedMessagesRead
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateDeleteMessages : ITLMultiPts
-    {
         public override IList<int> GetPts()
         {
             return TLUtils.GetPtsRange(Pts, PtsCount);
         }
     }
 
-    public partial class TLUpdateUserTyping
+    public partial class TLUpdateEncryptedMessagesRead : TLUpdateBase
     {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateChatUserTyping
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateEncryptedChatTyping
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateChatParticipants
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateUserStatus
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateUserName
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateUserPhoto
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateContactRegistered
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateContactLink
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    //public partial class TLUpdateActivation
-    //{
-    //    public override IList<int> GetPts()
-    //    {
-    //        return new List<int>();
-    //    }
-    //}
-
-    //public partial class TLUpdateNewAuthorization
-    //{
-    //    public override IList<int> GetPts()
-    //    {
-    //        return new List<int>();
-    //    }
-    //}
-
-    public partial class TLUpdateDCOptions
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateNotifySettings
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateUserBlocked
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdatePrivacy
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateUserPhone
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateServiceNotification
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-    }
-
-    public partial class TLUpdateWebPage : ITLMultiPts
-    {
-        public override IList<int> GetPts()
-        {
-            return TLUtils.GetPtsRange(Pts, PtsCount);
-        }
-    }
-
-    public partial class TLUpdateChannelTooLong
-    {
-        public override IList<int> GetPts()
-        {
-            return new List<int>();
-        }
-
         public override string ToString()
         {
-            return Logs.Log.Format("TLUpdateChannelTooLong channel_id={0} pts={1} flags={2}", ChannelId, Pts, Flags);
+            return string.Format("{0} chat_id={1} max_date={2} date={3}", GetType().Name, ChatId, MaxDate, Date);
+        }
+
+        public override IList<int> GetPts()
+        {
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateEditMessage : ITLMultiPts
+    public partial class TLUpdateDeleteMessages : TLUpdateBase, ITLMultiPts
     {
         public override IList<int> GetPts()
         {
@@ -285,7 +124,135 @@ namespace Telegram.Api.TL
         }
     }
 
-    public partial class TLUpdateNewChannelMessage : ITLMultiChannelPts
+    public partial class TLUpdateUserTyping : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChatUserTyping : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateEncryptedChatTyping : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChatParticipants : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateUserStatus : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateUserName : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateUserPhoto : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateContactRegistered : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateContactLink : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateNewAuthorization : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateDCOptions : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateNotifySettings : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateUserBlocked : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdatePrivacy : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateUserPhone : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateServiceNotification : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateWebPage : TLUpdateBase, ITLMultiPts
     {
         public override IList<int> GetPts()
         {
@@ -293,15 +260,169 @@ namespace Telegram.Api.TL
         }
     }
 
-    public partial class TLUpdateReadChannelInbox
+    public partial class TLUpdateChannelTooLong : TLUpdateBase
     {
+        public override string ToString()
+        {
+            return string.Format("TLUpdateChannelTooLong channel_id={0} pts={1} flags={2}", ChannelId, Pts, Flags);
+        }
+
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateDeleteChannelMessages : ITLMultiChannelPts
+    public partial class TLUpdateChannelGroup : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateNewChannelMessage : TLUpdateBase, ITLMultiChannelPts
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateReadChannelInbox : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateDeleteChannelMessages : TLUpdateBase, ITLMultiChannelPts
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChannelMessageViews : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChannel : TLUpdateBase
+    {
+        public override string ToString()
+        {
+            return "TLUpdateChannel channel_id=" + ChannelId;
+        }
+
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChatAdmins : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChatParticipantAdmin : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateNewStickerSet : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateStickerSetsOrder : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateStickerSets : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateSavedGifs : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateBotInlineQuery : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateBotInlineSend : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateEditChannelMessage : TLUpdateBase, ITLMultiChannelPts
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChannelPinnedMessage : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateBotCallbackQuery : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateInlineBotCallbackQuery : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateEditMessage : TLUpdateBase, ITLMultiPts
     {
         public override IList<int> GetPts()
         {
@@ -309,52 +430,168 @@ namespace Telegram.Api.TL
         }
     }
 
-    public partial class TLUpdateEditChannelMessage : ITLMultiChannelPts
+    public partial class TLUpdateReadChannelOutbox : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-
-    public partial class TLUpdateChannelWebPage : ITLMultiChannelPts
+    public partial class TLUpdateDraftMessage : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>(); //TLUtils.GetPtsRange(Pts, PtsCount);
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateChannelMessageViews
+    public partial class TLUpdateReadFeaturedStickers : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateChannel
+    public partial class TLUpdateRecentStickers : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateChatAdmins
+    public partial class TLUpdateConfig : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateChatParticipantAdmin
+    public partial class TLUpdatePtsChanged : TLUpdateBase
     {
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChannelWebPage : TLUpdateBase, ITLMultiChannelPts
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0]; //TLUtils.GetPtsRange(Pts, PtsCount);
+        }
+    }
+
+    public partial class TLUpdatePhoneCall : TLUpdateBase
+    {
+        public override string ToString()
+        {
+            return string.Format("TLUpdatePhoneCall PhoneCall={0}", PhoneCall);
+        }
+
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateDialogPinned : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdatePinnedDialogs : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateBotWebhookJSON : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateBotWebhookJSONQuery : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateBotShippingQuery : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateBotPrecheckoutQuery : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateLangPackTooLong : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateLangPack : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateFavedStickers : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateContactsReset : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChannelReadMessagesContents : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
+        }
+    }
+
+    public partial class TLUpdateChannelAvailableMessages : TLUpdateBase
+    {
+        public override IList<int> GetPts()
+        {
+            return new int[0];
         }
     }
 }
--- Unigram/Unigram.Api/TL/Partial/TLUpdatesBase.Partial.cs
+++ Unigram/Unigram.Api/TL/Partial/TLUpdatesBase.Partial.cs
@@ -6,83 +6,106 @@ using System.Threading.Tasks;
 
 namespace Telegram.Api.TL
 {
-    public abstract partial class TLUpdatesBase
+    public abstract partial class TLUpdatesBase : TLObject
     {
         public abstract IList<int> GetSeq();
 
         public abstract IList<int> GetPts();
     }
 
-    public partial class TLUpdatesTooLong
+    public partial class TLUpdatesTooLong : TLUpdatesBase
     {
         public override IList<int> GetSeq()
         {
-            return new List<int>();
+            return new int[0];
         }
 
         public override IList<int> GetPts()
         {
-            return new List<int>();
+            return new int[0];
         }
     }
 
-    public partial class TLUpdateShortSentMessage : ITLMultiPts
+    public partial class TLUpdateShortSentMessage : TLUpdatesBase, ITLMultiPts
     {
         public override IList<int> GetSeq()
         {
-            return new List<int>();
+            return new int[0];
         }
 
         public override IList<int> GetPts()
         {
             return TLUtils.GetPtsRange(Pts, PtsCount);
         }
+
+        public override string ToString()
+        {
+            //return string.Format("TLUpdateShortSentMessage id={0} media={1} flags={2}", Id, Media, TLMessageBase.MessageFlagsString(Flags));
+            return string.Format("TLUpdateShortSentMessage id={0} media={1} flags={2}", Id, Media, Flags);
+        }
     }
 
-    public partial class TLUpdateShortMessage : ITLMultiPts
+    public partial class TLUpdateShortMessage : TLUpdatesBase, ITLMultiPts
     {
         public bool IsUnread { get; set; } = true;
 
         public override IList<int> GetSeq()
         {
-            return new List<int>();
+            return new int[0];
         }
 
         public override IList<int> GetPts()
         {
             return TLUtils.GetPtsRange(Pts, PtsCount);
         }
+
+        public override string ToString()
+        {
+            return string.Format("TLUpdateShortMessage id={0} flags={1} user_id={2} message={3} pts={4} pts_count={5} date={6} fwd_from={7} via_bot_id={8} reply_to_msg_id={9} entities={10}", Id, Flags, UserId, Message.Substring(0, Math.Min(Message.Length, 5)), Pts, PtsCount, Date, FwdFrom, ViaBotId, ReplyToMsgId, Entities);
+            //return string.Format("TLUpdateShortMessage id={0} flags={1} user_id={2} message={3} pts={4} pts_count={5} date={6} fwd_from={7} via_bot_id={8} reply_to_msg_id={9} entities={10}", Id, TLMessageBase.MessageFlagsString(Flags), UserId, Message.Substring(0, Math.Min(Message.Length, 5)), Pts, PtsCount, Date, FwdFrom, ViaBotId, ReplyToMsgId, Entities);
+        }
     }
 
-    public partial class TLUpdateShortChatMessage : ITLMultiPts
+    public partial class TLUpdateShortChatMessage : TLUpdatesBase, ITLMultiPts
     {
         public bool IsUnread { get; set; } = true;
 
         public override IList<int> GetSeq()
         {
-            return new List<int>();
+            return new int[0];
         }
 
         public override IList<int> GetPts()
         {
             return TLUtils.GetPtsRange(Pts, PtsCount);
         }
+
+        public override string ToString()
+        {
+            return string.Format("TLUpdateShortChatMessage id={0} flags={1} from_id={2} message={3} pts={4} pts_count={5} date={6} fwd_from={7} reply_to_msg_id={8} via_bot_id={9} entities={10}", Id, Flags, FromId, Message.Substring(0, Math.Min(Message.Length, 5)), Pts, PtsCount, Date, FwdFrom, ViaBotId, ReplyToMsgId, Entities);
+            //return string.Format("TLUpdateShortChatMessage id={0} flags={1} from_id={2} message={3} pts={4} pts_count={5} date={6} fwd_from={7} reply_to_msg_id={8} via_bot_id={9} entities={10}", Id, TLMessageBase.MessageFlagsString(Flags), FromId, Message.Substring(0, Math.Min(Message.Length, 5)), Pts, PtsCount, Date, FwdFrom, ViaBotId, ReplyToMsgId, Entities);
+        }
     }
 
-    public partial class TLUpdateShort
+    public partial class TLUpdateShort : TLUpdatesBase
     {
         public override IList<int> GetSeq()
         {
-            return new List<int>();
+            return new int[0];
         }
 
         public override IList<int> GetPts()
         {
             return Update.GetPts();
         }
+
+        public override string ToString()
+        {
+            return "TLUpdatesShort update=" + Update;
+        }
     }
 
-    public partial class TLUpdates
+    public partial class TLUpdates : TLUpdatesBase
     {
         public override IList<int> GetSeq()
         {
@@ -93,9 +116,22 @@ namespace Telegram.Api.TL
         {
             return Updates.SelectMany(x => x.GetPts()).ToList();
         }
+
+        public override string ToString()
+        {
+            var info = new StringBuilder();
+            info.AppendLine("TLUpdates");
+
+            for (var i = 0; i < Updates.Count; i++)
+            {
+                info.AppendLine(Updates[i].ToString());
+            }
+
+            return info.ToString();
+        }
     }
 
-    public partial class TLUpdatesCombined
+    public partial class TLUpdatesCombined : TLUpdatesBase
     {
         public override IList<int> GetSeq()
         {
