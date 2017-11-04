6036d5e488bfb3d1fb335ca71a5ddd57e43a647c
Fela Ameghino <frayx@live.it>
2017-11-04 16:38:52 +0100

    Moved Unigram.Webview classes to Unigram.Tasks

--- Unigram/Unigram/Views/Payments/PaymentFormStep3Page.xaml.cs
+++ Unigram/Unigram/Views/Payments/PaymentFormStep3Page.xaml.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Unigram.Common;
 using Unigram.ViewModels.Payments;
-using Unigram.Webview;
+using Unigram.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
