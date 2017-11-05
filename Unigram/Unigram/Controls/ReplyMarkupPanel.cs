13a6250d5dbeda8068de24b9421ca01c47cfa7e5
Fela Ameghino <frayx@live.it>
2017-11-05 20:23:13 +0100

    Improved reply markup, fixed a bug related to gifs playback

--- Unigram/Unigram/Controls/ReplyMarkupPanel.cs
+++ Unigram/Unigram/Controls/ReplyMarkupPanel.cs
@@ -25,7 +25,7 @@ namespace Unigram.Controls
 
     public class ReplyMarkupPanel : Grid
     {
-        private double _keyboardHeight = 300;
+        private double _keyboardHeight = 260;
 
         //#region ReplyMarkup
 
@@ -86,19 +86,19 @@ namespace Unigram.Controls
                 var keyboard = ReplyMarkup as TLReplyKeyboardMarkup;
                 if (keyboard.IsResize)
                 {
-                    Height = double.NaN;
+                    scroll.Height = double.NaN;
                     scroll.MaxHeight = _keyboardHeight;
                 }
                 else
                 {
-                    Height = _keyboardHeight;
-                    scroll.MaxHeight = double.PositiveInfinity;
+                    scroll.Height = _keyboardHeight;
+                    scroll.MaxHeight = _keyboardHeight;
                 }
             }
             else if (ReplyMarkup is TLReplyKeyboardHide && !inline && Parent is ScrollViewer scroll2)
             {
-                Height = 0;
-                scroll2.MaxHeight = double.PositiveInfinity;
+                scroll2.Height = 0;
+                scroll2.MaxHeight = _keyboardHeight;
             }
         }
 
@@ -132,28 +132,6 @@ namespace Unigram.Controls
             {
                 receipt = invoiceMedia.HasReceiptMsgId;
             }
-
-            //if (receipt)
-            //{
-            //    var panel = new Grid();
-            //    panel.HorizontalAlignment = HorizontalAlignment.Stretch;
-            //    panel.VerticalAlignment = VerticalAlignment.Stretch;
-            //    panel.Margin = new Thickness(-2, 0, -2, 0);
-
-            //    var button = new GlyphButton();
-            //    button.DataContext = new TLKeyboardButtonBuy();
-            //    button.Content = "Receipt";
-            //    button.Margin = new Thickness(2, 2, 2, 0);
-            //    button.HorizontalAlignment = HorizontalAlignment.Stretch;
-            //    button.VerticalAlignment = VerticalAlignment.Stretch;
-            //    button.Click += Button_Click;
-            //    button.Style = App.Current.Resources["ReplyInlineMarkupButtonStyle"] as Style;
-
-            //    panel.Children.Add(button);
-            //    Children.Add(panel);
-
-            //    return;
-            //}
 
             if (rows != null && ((inline && newValue is TLReplyInlineMarkup) || (!inline && newValue is TLReplyKeyboardMarkup)))
             {
@@ -202,13 +180,13 @@ namespace Unigram.Controls
                             button.Content = "Receipt";
                         }
 
-                        Grid.SetColumn(button, i);
+                        SetColumn(button, i);
 
                         panel.ColumnDefinitions.Add(new ColumnDefinition());
                         panel.Children.Add(button);
                     }
 
-                    Grid.SetRow(panel, j);
+                    SetRow(panel, j);
 
                     RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, resize ? GridUnitType.Auto : GridUnitType.Star) });
                     Children.Add(panel);
@@ -234,8 +212,7 @@ namespace Unigram.Controls
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
-            var btn = button.DataContext as TLKeyboardButtonBase;
-            if (btn != null)
+            if (button.DataContext is TLKeyboardButtonBase btn)
             {
                 ButtonClick?.Invoke(this, new ReplyMarkupButtonClickEventArgs(btn));
             }
