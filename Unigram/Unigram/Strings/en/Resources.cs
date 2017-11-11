64bc4686b342f16c5e8e205691d0a07d9abcb5c6
Callum Moffat <13494563+callummoffat@users.noreply.github.com>
2017-11-12 10:20:58 +1300

    More localization

--- Unigram/Unigram/Strings/en/Resources.cs
+++ Unigram/Unigram/Strings/en/Resources.cs
@@ -17,7 +17,7 @@
 // 	This code contains a helper class exposing property representations
 // 	of the string resources defined in the specified .ResW file
 // 
-// 	Generated: 11/12/2017 10:03:48
+// 	Generated: 11/12/2017 10:20:45
 // </auto-generatedInfo>
 // --------------------------------------------------------------------------------------------------
 namespace Unigram.Strings
