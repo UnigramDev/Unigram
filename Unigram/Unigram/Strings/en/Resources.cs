9d5c562fcae56a0122cdc85d66304d35ac08bd01
Callum Moffat <13494563+callummoffat@users.noreply.github.com>
2017-11-14 10:58:26 +1300

    Localized DialogPage overflow menu flyout

--- Unigram/Unigram/Strings/en/Resources.cs
+++ Unigram/Unigram/Strings/en/Resources.cs
@@ -17,7 +17,7 @@
 // 	This code contains a helper class exposing property representations
 // 	of the string resources defined in the specified .ResW file
 // 
-// 	Generated: 11/14/2017 10:41:29
+// 	Generated: 11/14/2017 10:58:00
 // </auto-generatedInfo>
 // --------------------------------------------------------------------------------------------------
 namespace Unigram.Strings
