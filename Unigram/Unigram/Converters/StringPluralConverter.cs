11431f7e5650d57f050c2ce99748eef5bf4e1f21
Fela Ameghino <frayx@live.it>
2017-11-15 22:43:51 +0100

    Added base converter for language declesions

---
+++ Unigram/Unigram/Converters/StringPluralConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unigram.Common;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Data;
+
+namespace Unigram.Converters
+{
+    public class StringPluralConverter : IValueConverter
+    {
+        private readonly ResourceLoader _loader;
+
+        public StringPluralConverter()
+        {
+            _loader = ResourceLoader.GetForViewIndependentUse("Android");
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (parameter is string format)
+            {
+                // TODO: declesion
+
+                return string.Format(_loader.GetString(LocaleHelper.Pluralize(format, System.Convert.ToInt32(value))), value);
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
