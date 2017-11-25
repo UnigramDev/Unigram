bad62b188ef7b13e27ce8d546ccdbf37f2d8bb94
Fela Ameghino <frayx@live.it>
2017-11-25 19:07:50 +0100

    Localization fix

--- Unigram/Unigram/ViewModels/Users/UserPhotosViewModel.cs
+++ Unigram/Unigram/ViewModels/Users/UserPhotosViewModel.cs
@@ -81,7 +81,7 @@ namespace Unigram.ViewModels.Users
 
         protected override async void DeleteExecute()
         {
-            var confirm = await TLMessageDialog.ShowAsync("Do you want to delete this photo?", "Delete", "OK", "Cancel");
+            var confirm = await TLMessageDialog.ShowAsync(Strings.Android.AreYouSureDeletePhoto, Strings.Android.AppName, Strings.Android.OK, Strings.Android.Cancel);
             if (confirm == ContentDialogResult.Primary && _selectedItem is GalleryPhotoItem item)
             {
                 //var response = await ProtoService.UpdateProfilePhotoAsync(new TLInputPhotoEmpty());
