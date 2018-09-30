e9b996b2c072f3b993701b86494085ccc058187a
Fela Ameghino <frayx@live.it>
2018-09-30 22:56:21 +0200

    Fixed some UI glitches on RS5 due to some resource change

--- Unigram/Unigram/Controls/Views/AttachedStickersView.xaml.cs
+++ Unigram/Unigram/Controls/Views/AttachedStickersView.xaml.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Telegram.Td.Api;
 using Template10.Utils;
+using Unigram.Common;
 using Unigram.Native;
 using Unigram.ViewModels;
 using Unigram.Views;
@@ -23,13 +25,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Unigram.Controls.Views
 {
-    public sealed partial class AttachedStickersView : ContentDialogBase
+    public sealed partial class AttachedStickersView : ContentDialog
     {
         public AttachedStickersViewModel ViewModel => DataContext as AttachedStickersViewModel;
 
@@ -37,6 +38,8 @@ namespace Unigram.Controls.Views
         {
             InitializeComponent();
             DataContext = TLContainer.Current.Resolve<AttachedStickersViewModel>();
+
+            SecondaryButtonText = Strings.Resources.Close;
 
             //Loaded += async (s, args) =>
             //{
@@ -59,7 +62,15 @@ namespace Unigram.Controls.Views
             return context;
         }
 
-        public IAsyncOperation<ContentDialogBaseResult> ShowAsync(long parameter)
+        public Task<ContentDialogResult> ShowAsync(IList<StickerSetInfo> parameter)
+        {
+            ViewModel.IsLoading = false;
+            ViewModel.Items.ReplaceWith(parameter);
+
+            return this.ShowQueuedAsync();
+        }
+
+        public Task<ContentDialogResult> ShowAsync(long parameter)
         {
             ViewModel.IsLoading = true;
             ViewModel.Items.Clear();
@@ -72,117 +83,7 @@ namespace Unigram.Controls.Views
             });
 
             Loaded += handler;
-            return ShowAsync();
-        }
-
-        public IAsyncOperation<ContentDialogBaseResult> ShowAsync(IList<StickerSetInfo> parameter)
-        {
-            ViewModel.IsLoading = false;
-            ViewModel.Items.ReplaceWith(parameter);
-
-            return ShowAsync();
-        }
-
-        private Border LineAccent;
-
-        private ScrollViewer _scrollingHost;
-
-        private SpriteVisual _backgroundVisual;
-        private ExpressionAnimation _expression;
-        private ExpressionAnimation _expressionClip;
-
-        private void GridView_Loaded(object sender, RoutedEventArgs e)
-        {
-            var scroll = List.Descendants<ScrollViewer>().FirstOrDefault() as ScrollViewer;
-            if (scroll != null)
-            {
-                _scrollingHost = scroll;
-                _scrollingHost.ChangeView(null, 0, null, true);
-                scroll.ViewChanged += Scroll_ViewChanged;
-                Scroll_ViewChanged(scroll, null);
-
-                var brush = App.Current.Resources["SystemControlBackgroundChromeMediumLowBrush"] as SolidColorBrush;
-                var props = ElementCompositionPreview.GetScrollViewerManipulationPropertySet(scroll);
-
-                if (_backgroundVisual == null)
-                {
-                    _backgroundVisual = ElementCompositionPreview.GetElementVisual(BackgroundPanel).Compositor.CreateSpriteVisual();
-                    ElementCompositionPreview.SetElementChildVisual(BackgroundPanel, _backgroundVisual);
-                }
-
-                _backgroundVisual.Brush = _backgroundVisual.Compositor.CreateColorBrush(brush.Color);
-                _backgroundVisual.Size = new System.Numerics.Vector2((float)BackgroundPanel.ActualWidth, (float)BackgroundPanel.ActualHeight);
-                _backgroundVisual.Clip = _backgroundVisual.Compositor.CreateInsetClip();
-
-                _expression = _expression ?? _backgroundVisual.Compositor.CreateExpressionAnimation("Max(Maximum, Scrolling.Translation.Y)");
-                _expression.SetReferenceParameter("Scrolling", props);
-                _expression.SetScalarParameter("Maximum", -(float)BackgroundPanel.Margin.Top + 1);
-                _backgroundVisual.StopAnimation("Offset.Y");
-                _backgroundVisual.StartAnimation("Offset.Y", _expression);
-
-                _expressionClip = _expressionClip ?? _backgroundVisual.Compositor.CreateExpressionAnimation("Min(0, Maximum - Scrolling.Translation.Y)");
-                _expressionClip.SetReferenceParameter("Scrolling", props);
-                _expressionClip.SetScalarParameter("Maximum", -(float)BackgroundPanel.Margin.Top + 1);
-                _backgroundVisual.Clip.StopAnimation("Offset.Y");
-                _backgroundVisual.Clip.StartAnimation("Offset.Y", _expressionClip);
-            }
-
-            var panel = List.ItemsPanelRoot as ItemsWrapGrid;
-            if (panel != null)
-            {
-                panel.SizeChanged += (s, args) =>
-                {
-                    Scroll_ViewChanged(scroll, null);
-                };
-            }
-        }
-
-        private void GroupHeader_Loaded(object sender, RoutedEventArgs e)
-        {
-            var groupHeader = sender as Grid;
-            if (groupHeader != null && groupHeader.DataContext is StickerSetInfo covered && covered == ViewModel.Items[0])
-            {
-                LineAccent = groupHeader.FindName("LineAccent") as Border;
-
-                if (_scrollingHost != null)
-                {
-                    Scroll_ViewChanged(_scrollingHost, null);
-                }
-            }
-        }
-
-        private void Scroll_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
-        {
-            var scroll = sender as ScrollViewer;
-            var accent = 0;
-            var bottom = 1;
-
-            if (scroll.VerticalOffset < BackgroundPanel.Margin.Top)
-            {
-                accent = 1;
-            }
-            if (scroll.VerticalOffset == scroll.ScrollableHeight)
-            {
-                bottom = 0;
-            }
-
-            //if (LineTop.BorderThickness.Bottom != top)
-            //{
-            //    if (top == 0)
-            //    {
-            //        MaskTitleAndStatusBar();
-            //    }
-            //    else
-            //    {
-            //        SetupTitleAndStatusBar();
-            //    }
-            //}
-
-            if (LineAccent != null)
-            {
-                LineAccent.BorderThickness = new Thickness(0, accent, 0, 0);
-                LineBottom.BorderThickness = new Thickness(0, bottom, 0, 0);
-            }
+            return this.ShowQueuedAsync();
         }
 
         // SystemControlBackgroundChromeMediumLowBrush
@@ -193,9 +94,9 @@ namespace Unigram.Controls.Views
             var backgroundBrush = Application.Current.Resources["SystemControlBackgroundChromeMediumLowBrush"] as SolidColorBrush;
             var foregroundBrush = Application.Current.Resources["SystemControlForegroundBaseHighBrush"] as SolidColorBrush;
 
-            titlebar.BackgroundColor = backgroundBrush.Color;
+            //titlebar.BackgroundColor = backgroundBrush.Color;
             titlebar.ForegroundColor = foregroundBrush.Color;
-            titlebar.ButtonBackgroundColor = backgroundBrush.Color;
+            //titlebar.ButtonBackgroundColor = backgroundBrush.Color;
             titlebar.ButtonForegroundColor = foregroundBrush.Color;
 
             if (ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
@@ -206,70 +107,9 @@ namespace Unigram.Controls.Views
             }
         }
 
-        private void List_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            var itemWidth = (e.NewSize.Width - 24) / 5d;
-            var minHeigth = itemWidth * 3d - 12 + 48;
-            var top = Math.Max(0, e.NewSize.Height - minHeigth);
-
-            if (!IsFullScreenMode())
-            {
-                top = 0;
-            }
-
-            if (top == 0)
-            {
-                Header.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                Header.Visibility = Visibility.Visible;
-            }
-
-            Header.Height = top;
-
-            BackgroundPanel.Height = e.NewSize.Height;
-            BackgroundPanel.Margin = new Thickness(0, top, 0, -top);
-
-            if (_backgroundVisual != null && _expression != null && _expressionClip != null)
-            {
-                var brush = App.Current.Resources["SystemControlBackgroundChromeMediumLowBrush"] as SolidColorBrush;
-
-                _backgroundVisual.Brush = _backgroundVisual.Compositor.CreateColorBrush(brush.Color);
-                _backgroundVisual.Size = new System.Numerics.Vector2((float)e.NewSize.Width, (float)e.NewSize.Height);
-                _backgroundVisual.Clip = _backgroundVisual.Compositor.CreateInsetClip();
-
-                _expression.SetScalarParameter("Maximum", -(float)top + 1);
-                _backgroundVisual.StopAnimation("Offset.Y");
-                _backgroundVisual.StartAnimation("Offset.Y", _expression);
-
-                _expressionClip.SetScalarParameter("Maximum", -(float)top + 1);
-                _backgroundVisual.Clip.StopAnimation("Offset.Y");
-                _backgroundVisual.Clip.StartAnimation("Offset.Y", _expressionClip);
-            }
-        }
-
-        //protected override void UpdateView(Rect bounds)
-        //{
-        //    if (BackgroundElement == null) return;
-
-        //    BackgroundElement.MinHeight = bounds.Height;
-        //    BackgroundElement.BorderThickness = new Thickness(0);
-        //}
-
-        private void LightDismiss_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            Hide(ContentDialogBaseResult.None);
-        }
-
-        private void Close_Click(object sender, RoutedEventArgs e)
-        {
-            Hide(ContentDialogBaseResult.Cancel);
-        }
-
         private async void List_ItemClick(object sender, ItemClickEventArgs e)
         {
-            Hide(ContentDialogBaseResult.OK);
+            Hide();
 
             var item = e.ClickedItem as Sticker;
             if (item != null)
@@ -304,10 +144,7 @@ namespace Unigram.Controls.Views
                 var file = sticker.Thumbnail.Photo;
                 if (file.Local.IsDownloadingCompleted)
                 {
-                    var temp = await StorageFile.GetFileFromPathAsync(file.Local.Path);
-                    var buffer = await FileIO.ReadBufferAsync(temp);
-
-                    content.Source = WebPImage.DecodeFromBuffer(buffer);
+                    content.Source = await PlaceholderHelper.GetWebpAsync(file.Local.Path);
                 }
                 else if (file.Local.CanBeDownloaded && !file.Local.IsDownloadingActive)
                 {
