a9a1a4934a726bcc31f585a9b70df6f0359fb014
Fela Ameghino <frayx@live.it>
2018-10-04 22:10:34 +0200

    Refactoring

--- Unigram/Unigram/ViewModels/ChatsViewModel.cs
+++ Unigram/Unigram/ViewModels/ChatsViewModel.cs
@@ -360,46 +360,4 @@ namespace Unigram.ViewModels
             IsPublic = pub;
         }
     }
-
-    public class TopChatsCollection : MvxObservableCollection<Chat>, ISupportIncrementalLoading
-    {
-        private readonly IProtoService _protoService;
-        private readonly TopChatCategory _category;
-        private readonly int _limit;
-
-        private bool _hasMore = false;
-
-        public TopChatsCollection(IProtoService protoService, TopChatCategory category, int limit)
-        {
-            _protoService = protoService;
-            _category = category;
-            _limit = limit;
-        }
-
-        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
-        {
-            return AsyncInfo.Run(async token =>
-            {
-                count = 0;
-
-                var response = await _protoService.SendAsync(new GetTopChats(_category, _limit));
-                if (response is Telegram.Td.Api.Chats chats)
-                {
-                    foreach (var id in chats.ChatIds)
-                    {
-                        var chat = _protoService.GetChat(id);
-                        if (chat != null)
-                        {
-                            Add(chat);
-                            count++;
-                        }
-                    }
-                }
-
-                return new LoadMoreItemsResult { Count = count };
-            });
-        }
-
-        public bool HasMoreItems => _hasMore;
-    }
 }
