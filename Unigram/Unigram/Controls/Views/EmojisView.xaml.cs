88055c377a9ed4caff2188d6707560735b8ecb20
Fela Ameghino <frayx@live.it>
2019-05-05 20:26:34 +0200

    New emojis panel

--- Unigram/Unigram/Controls/Views/EmojisView.xaml.cs
+++ Unigram/Unigram/Controls/Views/EmojisView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinqToVisualTree;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Unigram.Common;
+using Unigram.Services;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
@@ -36,26 +38,72 @@ namespace Unigram.Controls.Views
                 shadow.Size = args.NewSize.ToVector2();
             };
 
-            Pivot.ItemsSource = Emoji.Emojis.Keys;
-            Toolbar.ItemsSource = Emoji.Emojis.Keys;
+            EmojisViewSource.Source = Emoji.Items.ToArray();
+            Toolbar.ItemsSource = Emoji.Items.ToArray();
             Toolbar.SelectedIndex = 0;
+        }
 
-            //Follodf.ShowMode = FlyoutShowMode.Transient;
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var scrollingHost = List.Descendants<ScrollViewer>().FirstOrDefault() as ScrollViewer;
+            if (scrollingHost != null)
+            {
+                // Syncronizes GridView with the toolbar ListView
+                scrollingHost.ViewChanged += ScrollingHost_ViewChanged;
+                ScrollingHost_ViewChanged(null, null);
+            }
         }
 
         public void SetView(bool widget)
         {
             VisualStateManager.GoToState(this, widget ? "FilledState" : "NarrowState", false);
+
+            if (Toolbar.ItemsSource is EmojiGroup[] groups)
+            {
+                var microsoft = string.Equals(SettingsService.Current.Appearance.EmojiSetId, "microsoft");
+
+                if (groups.Length == Emoji.Items.Count && microsoft)
+                {
+                    EmojisViewSource.Source = Emoji.Items.Take(Emoji.Items.Count - 1).ToArray();
+                    Toolbar.ItemsSource = Emoji.Items.Take(Emoji.Items.Count - 1).ToArray();
+                }
+                else if (groups.Length == Emoji.Items.Count - 1 && !microsoft)
+                {
+                    EmojisViewSource.Source = Emoji.Items.ToArray();
+                    Toolbar.ItemsSource = Emoji.Items.ToArray();
+                }
+            }
+        }
+
+        private void ScrollingHost_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            var scrollingHost = List.ItemsPanelRoot as ItemsWrapGrid;
+            if (scrollingHost != null)
+            {
+                var first = List.ContainerFromIndex(scrollingHost.FirstVisibleIndex);
+                if (first != null)
+                {
+                    var header = List.GroupHeaderContainerFromItemContainer(first) as GridViewHeaderItem;
+                    if (header != null && header != Toolbar.SelectedItem)
+                    {
+                        Toolbar.SelectedItem = header.Content;
+                        Toolbar.ScrollIntoView(header.Content);
+                    }
+                }
+            }
         }
 
         private void Toolbar_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Pivot.SelectedIndex = Toolbar.SelectedIndex;
+            //Pivot.SelectedIndex = Toolbar.SelectedIndex;
         }
 
         private void Toolbar_ItemClick(object sender, ItemClickEventArgs e)
         {
-
+            if (e.ClickedItem is EmojiGroup group)
+            {
+                List.ScrollIntoView(e.ClickedItem, ScrollIntoViewAlignment.Leading);
+            }
         }
 
         private void ListView_ItemClick(object sender, ItemClickEventArgs e)
@@ -66,55 +114,6 @@ namespace Unigram.Controls.Views
         private void Switch_Click(object sender, RoutedEventArgs e)
         {
             Switch?.Invoke(this, EventArgs.Empty);
-        }
-
-        private void ListView_Loaded(object sender, RoutedEventArgs e)
-        {
-            var root = sender as UserControl;
-            var container = Pivot.ContainerFromItem(root.DataContext) as PivotItem;
-            var index = Pivot.IndexFromContainer(container);
-
-            if (index != Pivot.SelectedIndex)
-            {
-                return;
-            }
-
-            var list = root.FindName("List") as GridView;
-            if (list == null || list.ItemsSource != null)
-            {
-                return;
-            }
-
-            if (container.Content is EmojiGroup source)
-            {
-                list.ItemsSource = Emoji.Emojis[source];
-            }
-        }
-
-        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var container = Pivot.ContainerFromIndex(Pivot.SelectedIndex) as PivotItem;
-            if (container == null)
-            {
-                return;
-            }
-
-            var root = container.ContentTemplateRoot as UserControl;
-            if (root == null)
-            {
-                return;
-            }
-
-            var list = root.FindName("List") as GridView;
-            if (list == null || list.ItemsSource != null)
-            {
-                return;
-            }
-
-            if (container.Content is EmojiGroup source)
-            {
-                list.ItemsSource = Emoji.Emojis[source];
-            }
         }
     }
 }
