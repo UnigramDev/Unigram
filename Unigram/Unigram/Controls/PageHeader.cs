6863a72a106d937219093e5b88da49723a50f5bb
Fela Ameghino <frayx@live.it>
2019-06-13 09:48:20 +0200

    Page level back button

--- Unigram/Unigram/Controls/PageHeader.cs
+++ Unigram/Unigram/Controls/PageHeader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Template10.Common;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Markup;
@@ -16,6 +17,56 @@ namespace Unigram.Controls
         {
             DefaultStyleKey = typeof(PageHeader);
         }
+
+        protected override void OnApplyTemplate()
+        {
+            var button = GetTemplateChild("BackButton") as Button;
+            if (button != null)
+            {
+                button.Click += Back_Click;
+            }
+
+            base.OnApplyTemplate();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            var command = BackCommand;
+            if (command != null)
+            {
+                command.Execute(null);
+            }
+            else
+            {
+                BootStrapper.Current.RaiseBackRequested();
+            }
+        }
+
+        #region BackCommand
+
+        public ICommand BackCommand
+        {
+            get { return (ICommand)GetValue(BackCommandProperty); }
+            set { SetValue(BackCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackCommandProperty =
+            DependencyProperty.Register("BackCommand", typeof(ICommand), typeof(PageHeader), new PropertyMetadata(null));
+
+        #endregion
+
+        #region BackVisibility
+
+        public Visibility BackVisibility
+        {
+            get { return (Visibility)GetValue(BackVisibilityProperty); }
+            set { SetValue(BackVisibilityProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackVisibilityProperty =
+            DependencyProperty.Register("BackVisibility", typeof(Visibility), typeof(PageHeader), new PropertyMetadata(Visibility.Visible));
+
+        #endregion
 
         #region Text
 
@@ -123,6 +174,56 @@ namespace Unigram.Controls
         {
             DefaultStyleKey = typeof(ContentPageHeader);
         }
+
+        protected override void OnApplyTemplate()
+        {
+            var button = GetTemplateChild("BackButton") as Button;
+            if (button != null)
+            {
+                button.Click += Back_Click;
+            }
+
+            base.OnApplyTemplate();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            var command = BackCommand;
+            if (command != null)
+            {
+                command.Execute(null);
+            }
+            else
+            {
+                BootStrapper.Current.RaiseBackRequested();
+            }
+        }
+
+        #region BackCommand
+
+        public ICommand BackCommand
+        {
+            get { return (ICommand)GetValue(BackCommandProperty); }
+            set { SetValue(BackCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackCommandProperty =
+            DependencyProperty.Register("BackCommand", typeof(ICommand), typeof(ContentPageHeader), new PropertyMetadata(null));
+
+        #endregion
+
+        #region BackVisibility
+
+        public Visibility BackVisibility
+        {
+            get { return (Visibility)GetValue(BackVisibilityProperty); }
+            set { SetValue(BackVisibilityProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackVisibilityProperty =
+            DependencyProperty.Register("BackVisibility", typeof(Visibility), typeof(ContentPageHeader), new PropertyMetadata(Visibility.Visible));
+
+        #endregion
 
         #region IsLoading
 
