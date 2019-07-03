f845c7714b144008e9def4e0889c6a74e665844f
Fela Ameghino <frayx@live.it>
2019-07-03 10:58:26 +0200

    Group chat members by type

--- Unigram/Unigram/ViewModels/Supergroups/SupergroupMembersViewModel.cs
+++ Unigram/Unigram/ViewModels/Supergroups/SupergroupMembersViewModel.cs
@@ -6,22 +6,129 @@ using System.Threading.Tasks;
 using Telegram.Td.Api;
 using Unigram.Common;
 using Unigram.Services;
+using Unigram.ViewModels.Delegates;
 using Unigram.Views.Channels;
 using Unigram.Views.Chats;
 using Unigram.Views.Supergroups;
+using Windows.UI.Xaml.Navigation;
 
 namespace Unigram.ViewModels.Supergroups
 {
-    public class SupergroupMembersViewModel : SupergroupMembersViewModelBase
+    public class SupergroupMembersViewModel : TLViewModelBase, IDelegable<ISupergroupDelegate>
     {
+        public ISupergroupDelegate Delegate { get; set; }
+
         public SupergroupMembersViewModel(IProtoService protoService, ICacheService cacheService, ISettingsService settingsService, IEventAggregator aggregator)
-            : base(protoService, cacheService, settingsService, aggregator, new SupergroupMembersFilterRecent(), query => new SupergroupMembersFilterSearch(query))
+            : base(protoService, cacheService, settingsService, aggregator)
         {
             AddCommand = new RelayCommand(AddExecute);
 
             MemberPromoteCommand = new RelayCommand<ChatMember>(MemberPromoteExecute);
             MemberRestrictCommand = new RelayCommand<ChatMember>(MemberRestrictExecute);
             MemberRemoveCommand = new RelayCommand<ChatMember>(MemberRemoveExecute);
+        }
+
+
+        protected Chat _chat;
+        public Chat Chat
+        {
+            get
+            {
+                return _chat;
+            }
+            set
+            {
+                Set(ref _chat, value);
+            }
+        }
+
+        public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
+        {
+            var chatId = (long)parameter;
+
+            Chat = ProtoService.GetChat(chatId);
+
+            var chat = _chat;
+            if (chat == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (chat.Type is ChatTypeSupergroup supergroup)
+            {
+                var item = ProtoService.GetSupergroup(supergroup.SupergroupId);
+                var cache = ProtoService.GetSupergroupFull(supergroup.SupergroupId);
+
+                Delegate?.UpdateSupergroup(chat, item);
+
+                if (cache == null)
+                {
+                    ProtoService.Send(new GetSupergroupFullInfo(supergroup.SupergroupId));
+                }
+                else
+                {
+                    Delegate?.UpdateSupergroupFullInfo(chat, item, cache);
+                }
+
+                Members = new ChatMemberGroupedCollection(ProtoService, supergroup.SupergroupId);
+            }
+            else if (chat.Type is ChatTypeBasicGroup basicGroup)
+            {
+                var item = ProtoService.GetBasicGroup(basicGroup.BasicGroupId);
+
+                if (Delegate is IBasicGroupDelegate basicDelegate)
+                {
+                    basicDelegate.UpdateBasicGroup(chat, item);
+                }
+
+                Members = new ChatMemberGroupedCollection(ProtoService, chat.Id, string.Empty);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public void Find(string query)
+        {
+            var chat = _chat;
+            if (chat == null)
+            {
+                return;
+            }
+
+            if (chat.Type is ChatTypeSupergroup supergroup)
+            {
+                Search = new ChatMemberCollection(ProtoService, supergroup.SupergroupId, new SupergroupMembersFilterSearch(query));
+            }
+            else if (chat.Type is ChatTypeBasicGroup basicGroup)
+            {
+                Search = new ChatMemberCollection(ProtoService, chat.Id, query, null);
+            }
+        }
+
+        protected ChatMemberGroupedCollection _members;
+        public ChatMemberGroupedCollection Members
+        {
+            get
+            {
+                return _members;
+            }
+            set
+            {
+                Set(ref _members, value);
+            }
+        }
+
+        protected ChatMemberCollection _search;
+        public ChatMemberCollection Search
+        {
+            get
+            {
+                return _search;
+            }
+            set
+            {
+                Set(ref _search, value);
+            }
         }
 
         public RelayCommand AddCommand { get; }
