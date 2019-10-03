b2618fe8a2ad576f23141a8b226b97a5923db063
Fela Ameghino <frayx@live.it>
2019-10-03 09:13:59 +0200

    Added my rlottie porting

---
+++ Unigram/Unigram/Controls/LottieView.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using RLottie;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Graphics.DirectX;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Unigram.Controls
+{
+    [TemplatePart(Name = "Canvas", Type = typeof(CanvasAnimatedControl))]
+    public class LottieView : Control
+    {
+        private CanvasAnimatedControl Canvas;
+        private string CanvasPartName = "Canvas";
+
+        private string _source;
+        private Animation _animation;
+        private CanvasBitmap[] _cache;
+
+        private int _index;
+
+        private bool _isLoopingEnabled = true;
+
+        public LottieView()
+        {
+            DefaultStyleKey = typeof(LottieView);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            var canvas = GetTemplateChild(CanvasPartName) as CanvasAnimatedControl;
+            if (canvas == null)
+            {
+                return;
+            }
+
+            Canvas = canvas;
+            Canvas.Paused = true;
+            Canvas.Unloaded += OnUnloaded;
+            Canvas.CreateResources += OnCreateResources;
+            Canvas.Draw += OnDraw;
+
+            base.OnApplyTemplate();
+        }
+
+        public void Dispose()
+        {
+            var cache = _cache;
+            if (cache != null)
+            {
+                for (int i = 0; i < cache.Length; i++)
+                {
+                    if (cache[i] != null)
+                    {
+                        cache[i].Dispose();
+                        cache[i] = null;
+                    }
+                }
+            }
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            var canvas = Canvas;
+            if (canvas != null)
+            {
+                canvas.RemoveFromVisualTree();
+                Canvas = null;
+            }
+
+            Dispose();
+        }
+
+        private void OnCreateResources(CanvasAnimatedControl sender, CanvasCreateResourcesEventArgs args)
+        {
+            Dispose();
+
+            if (args.Reason == CanvasCreateResourcesReason.FirstTime)
+            {
+                OnSourceChanged(UriToPath(Source), _source);
+            }
+        }
+
+        private void OnDraw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+        {
+            if (_animation == null || _cache == null || _animation.TotalFrame != _cache.Length)
+            {
+                return;
+            }
+
+            var index = (int)(args.Timing.UpdateCount % _animation.TotalFrame);
+            if (index == _animation.TotalFrame - 1 && !_isLoopingEnabled)
+            {
+                sender.Paused = true;
+                sender.ResetElapsedTime();
+            }
+
+            if (_cache[index] == null)
+            {
+                var bytes = _animation.RenderSync(index);
+                _cache[index] = CanvasBitmap.CreateFromBytes(sender, bytes, 512, 512, DirectXPixelFormat.B8G8R8A8UIntNormalized);
+            }
+
+            args.DrawingSession.DrawImage(_cache[index], new Rect(0, 0, 160, 160));
+            _index = (_index + 1) % _animation.TotalFrame;
+        }
+
+        private void OnSourceChanged(string newValue, string oldValue)
+        {
+            if (newValue == null)
+            {
+                // TODO
+                return;
+            }
+
+            if (newValue == oldValue)
+            {
+                return;
+            }
+
+            var canvas = Canvas;
+            if (canvas == null)
+            {
+                return;
+            }
+
+            _source = newValue;
+            _animation = Animation.LoadFromFile(newValue);
+            _cache = new CanvasBitmap[_animation.TotalFrame];
+
+            canvas.Paused = true;
+            canvas.TargetElapsedTime = TimeSpan.FromSeconds(_animation.Duration / _animation.TotalFrame);
+
+            if (AutoPlay)
+            {
+                canvas.Paused = false;
+                canvas.ResetElapsedTime();
+                canvas.Invalidate();
+            }
+        }
+
+        private string UriToPath(Uri uri)
+        {
+            if (uri == null)
+            {
+                return null;
+            }
+
+            switch (uri.Scheme)
+            {
+                case "ms-appx":
+                    return Path.Combine(uri.Segments.Select(x => x.Trim('/')).ToArray());
+                case "ms-appdata":
+                    switch (uri.Host)
+                    {
+                        case "local":
+                            return Path.Combine(new[] { ApplicationData.Current.LocalFolder.Path }.Union(uri.Segments.Select(x => x.Trim('/'))).ToArray());
+                        case "temp":
+                            return Path.Combine(new[] { ApplicationData.Current.TemporaryFolder.Path }.Union(uri.Segments.Select(x => x.Trim('/'))).ToArray());
+                    }
+                    break;
+                case "file":
+                    return uri.AbsolutePath.Replace('/', '\\');
+            }
+
+            return null;
+        }
+
+        #region IsLoopingEnabled
+
+        public bool IsLoopingEnabled
+        {
+            get { return (bool)GetValue(IsLoopingEnabledProperty); }
+            set { SetValue(IsLoopingEnabledProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsLoopingEnabledProperty =
+            DependencyProperty.Register("IsLoopingEnabled", typeof(bool), typeof(LottieView), new PropertyMetadata(true, OnLoopingEnabledChanged));
+
+        private static void OnLoopingEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((LottieView)d)._isLoopingEnabled = (bool)e.NewValue;
+        }
+
+        #endregion
+
+        #region AutoPlay
+
+        public bool AutoPlay
+        {
+            get { return (bool)GetValue(AutoPlayProperty); }
+            set { SetValue(AutoPlayProperty, value); }
+        }
+
+        public static readonly DependencyProperty AutoPlayProperty =
+            DependencyProperty.Register("AutoPlay", typeof(bool), typeof(LottieView), new PropertyMetadata(true));
+
+        #endregion
+
+        #region Source
+
+        public Uri Source
+        {
+            get { return (Uri)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(Uri), typeof(LottieView), new PropertyMetadata(null));
+
+        #endregion
+    }
+}
--- Unigram/Unigram/Controls/Messages/Content/AnimatedStickerContent.xaml.cs
+++ Unigram/Unigram/Controls/Messages/Content/AnimatedStickerContent.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Toolkit.Uwp.UI.Lottie;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO.Compression;
 using System.Linq;
@@ -56,7 +55,7 @@ namespace Unigram.Controls.Messages.Content
 
         public void UpdateMessageContentOpened(MessageViewModel message) { }
 
-        public async void UpdateFile(MessageViewModel message, File file)
+        public void UpdateFile(MessageViewModel message, File file)
         {
             var sticker = GetContent(message.Content);
             if (sticker == null)
@@ -76,54 +75,13 @@ namespace Unigram.Controls.Messages.Content
 
             if (file.Local.IsDownloadingCompleted)
             {
-                //Texture.Source = await PlaceholderHelper.GetWebpAsync(file.Local.Path);
-                //Player.Source = LottieVisualSource.CreateFromString("file:///" + file.Local.Path); // new LottieVisualSource { UriSource = new Uri("file:///" + file.Local.Path) };
-
-                try
-                {
-                    var storage = await StorageFile.GetFileFromPathAsync(file.Local.Path);
-                    var content = await DecompressAsync(storage);
-                    var source = new LottieVisualSource();
-
-                    Player.Source = source;
-
-                    try
-                    {
-                        await source.SetSource(content);
-                    }
-                    catch { }
-
-                    content.Dispose();
-                }
-                catch
-                {
-                    // For some reason LottieVisualSource throws an exception on unsupported OS versions
-                }
-
+                LayoutRoot.Background = null;
+                Player.Source = new Uri("file:///" + file.Local.Path);
             }
             else if (file.Local.CanBeDownloaded && !file.Local.IsDownloadingActive)
             {
                 message.ProtoService.DownloadFile(file.Id, 1);
             }
-        }
-
-        private async Task<System.IO.MemoryStream> DecompressAsync(StorageFile file)
-        {
-            System.IO.MemoryStream text;
-
-            using (var originalFileStream = await System.IO.WindowsRuntimeStorageExtensions.OpenStreamForReadAsync(file))
-            {
-                var decompressedFileStream = new System.IO.MemoryStream();
-                using (var decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
-                {
-                    await decompressionStream.CopyToAsync(decompressedFileStream);
-                }
-
-                decompressedFileStream.Seek(0, System.IO.SeekOrigin.Begin);
-                text = decompressedFileStream;
-            }
-
-            return text;
         }
 
         private async void UpdateThumbnail(MessageViewModel message, File file)
