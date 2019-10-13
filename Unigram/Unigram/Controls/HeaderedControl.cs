b14a0dbab40caba5954d8bb667de0c0d60b73bf0
Fela Ameghino <frayx@live.it>
2019-10-13 17:28:45 +0200

    Trying to unify settings design (for easier redesign)

---
+++ Unigram/Unigram/Controls/HeaderedControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace Unigram.Controls
+{
+    public class HeaderedControl : ItemsControl
+    {
+        public HeaderedControl()
+        {
+            DefaultStyleKey = typeof(HeaderedControl);
+        }
+
+        #region Header
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(HeaderedControl), new PropertyMetadata(null));
+
+        #endregion
+
+        #region Footer
+
+        public string Footer
+        {
+            get { return (string)GetValue(FooterProperty); }
+            set { SetValue(FooterProperty, value); }
+        }
+
+        public static readonly DependencyProperty FooterProperty =
+            DependencyProperty.Register("Footer", typeof(string), typeof(HeaderedControl), new PropertyMetadata(null));
+
+        #endregion
+    }
+}
