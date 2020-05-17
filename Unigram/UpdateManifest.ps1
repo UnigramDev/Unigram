1f0c859f132254d6b47166c758c7f93680f62748
Fela Ameghino <frayx@live.it>
2020-05-17 21:15:10 +0200

    Don't save manifest if it hasn't changed

--- Unigram/UpdateManifest.ps1
+++ Unigram/UpdateManifest.ps1
@@ -31,12 +31,19 @@ $h["DEBUG"] = "38833FF26BA1D.UnigramExperimental"
 $h["RELEASE"] = "38833FF26BA1D.UnigramPreview"
 
 $identity = $document.GetElementsByTagName("Identity")[0]
+$original1 = $identity.Attributes["Name"].Value
+$original2 = $identity.Attributes["Version"].Value
+
 $identity.Attributes["Name"].Value = $h[$config]
 
 $version = $identity.Attributes["Version"].Value;
 $regex = [regex]'(?:(\d+)\.)(?:(\d+)\.)(?:(\d*?)\.\d+)'
 
 $identity.Attributes["Version"].Value = $regex.Replace($version, '$1.$2.{0}.0' -f $out)
+
+if ($original1 -eq $identity.Attributes["Name"].Value -and $original2 -eq $identity.Attributes["Version"].Value) {
+    exit
+}
 
 $h = @{}
 $h["DEBUG"] = "Unigram Experimental"
