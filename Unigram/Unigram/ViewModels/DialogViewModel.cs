7545bdac772bec1310283696fced4e2cdb122589
Fela Ameghino <frayx@live.it>
2020-06-05 20:25:14 +0200

    Fixed chat scrolling position

--- Unigram/Unigram/ViewModels/DialogViewModel.cs
+++ Unigram/Unigram/ViewModels/DialogViewModel.cs
@@ -1025,7 +1025,7 @@ namespace Unigram.ViewModels
                 }
             }
 
-            if (alignment == VerticalAlignment.Bottom)
+            if (alignment == VerticalAlignment.Bottom && pixel == null)
             {
                 pixel = int.MaxValue;
             }
@@ -1146,7 +1146,7 @@ namespace Unigram.ViewModels
                     if (maxId == chat.LastReadInboxMessageId && maxId == chat.LastMessage?.Id && alignment != VerticalAlignment.Center)
                     {
                         alignment = VerticalAlignment.Bottom;
-                        pixel = 12;
+                        pixel = null;
                     }
 
                     if (replied.Count > 0 && replied[0].Content is MessageChatUpgradeFrom chatUpgradeFrom)
