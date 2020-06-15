cf2d9c263df0e5fa1dbe9389a30d8b5986205a3f
Fela Ameghino <frayx@live.it>
2020-06-15 15:30:00 +0200

    Quiz timeout

--- Unigram/Unigram/Controls/Messages/Content/PollContent.xaml.cs
+++ Unigram/Unigram/Controls/Messages/Content/PollContent.xaml.cs
@@ -15,10 +15,23 @@ namespace Unigram.Controls.Messages.Content
         private MessageViewModel _message;
         public MessageViewModel Message => _message;
 
+        private DispatcherTimer _timeoutTimer;
+
         public PollContent(MessageViewModel message)
         {
             InitializeComponent();
             UpdateMessage(message);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            _timeoutTimer?.Stop();
+            _timeoutTimer = null;
         }
 
         public void UpdateMessage(MessageViewModel message)
@@ -29,6 +42,38 @@ namespace Unigram.Controls.Messages.Content
             if (poll == null)
             {
                 return;
+            }
+
+            if (poll.Poll.Type is PollTypeQuiz && poll.Poll.CloseDate != 0)
+            {
+                var now = DateTime.Now.ToTimestamp();
+
+                var diff = poll.Poll.CloseDate - now;
+                if (diff > 0)
+                {
+                    TimeoutLabel.Visibility = Visibility.Visible;
+                    Timeout.Text = TimeSpan.FromSeconds(diff).ToString("m\\:ss");
+
+                    if (_timeoutTimer == null)
+                    {
+                        _timeoutTimer = new DispatcherTimer();
+                        _timeoutTimer.Interval = TimeSpan.FromSeconds(1);
+                        _timeoutTimer.Tick += TimeoutTimer_Tick;
+                    }
+
+                    _timeoutTimer.Stop();
+                    _timeoutTimer.Start();
+                }
+                else
+                {
+                    _timeoutTimer?.Stop();
+                    TimeoutLabel.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                _timeoutTimer?.Stop();
+                TimeoutLabel.Visibility = Visibility.Collapsed;
             }
 
             var results = poll.Poll.IsClosed || poll.Poll.Options.Any(x => x.IsChosen);
@@ -141,6 +186,29 @@ namespace Unigram.Controls.Messages.Content
                 }
 
                 RecentVoters.Children.Add(picture);
+            }
+        }
+
+        private void TimeoutTimer_Tick(object sender, object e)
+        {
+            var poll = _message?.Content as MessagePoll;
+            if (poll == null)
+            {
+                _timeoutTimer.Stop();
+                return;
+            }
+
+            var now = DateTime.Now.ToTimestamp();
+
+            var diff = poll.Poll.CloseDate - now;
+            if (diff > 0)
+            {
+                Timeout.Text = TimeSpan.FromSeconds(diff).ToString("m\\:ss");
+            }
+            else
+            {
+                _timeoutTimer.Stop();
+                TimeoutLabel.Visibility = Visibility.Collapsed;
             }
         }
 
