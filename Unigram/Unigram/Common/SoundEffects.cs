9df1331867761d0573f4b6ea40db11d7e9a40b44
Fela Ameghino <frayx@live.it>
2020-06-18 13:15:25 +0200

    Try catch AudioGraph initialization for some reason

--- Unigram/Unigram/Common/SoundEffects.cs
+++ Unigram/Unigram/Common/SoundEffects.cs
@@ -9,33 +9,38 @@ namespace Unigram.Common
     {
         public static async void Play(SoundEffect effect)
         {
-            var settings = new AudioGraphSettings(AudioRenderCategory.SoundEffects);
-            settings.QuantumSizeSelectionMode = QuantumSizeSelectionMode.LowestLatency;
-
-            var result = await AudioGraph.CreateAsync(settings);
-            if (result.Status != AudioGraphCreationStatus.Success)
+            try
             {
-                return;
+                // This seems to fail in some conditions.
+                var settings = new AudioGraphSettings(AudioRenderCategory.SoundEffects);
+                settings.QuantumSizeSelectionMode = QuantumSizeSelectionMode.SystemDefault;
+
+                var result = await AudioGraph.CreateAsync(settings);
+                if (result.Status != AudioGraphCreationStatus.Success)
+                {
+                    return;
+                }
+
+                var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Audio/sent.mp3"));
+
+                var fileInputNodeResult = await result.Graph.CreateFileInputNodeAsync(file);
+                if (fileInputNodeResult.Status != AudioFileNodeCreationStatus.Success)
+                {
+                    return;
+                }
+
+                var deviceOutputNodeResult = await result.Graph.CreateDeviceOutputNodeAsync();
+                if (deviceOutputNodeResult.Status != AudioDeviceNodeCreationStatus.Success)
+                {
+                    return;
+                }
+
+                fileInputNodeResult.FileInputNode
+                    .AddOutgoingConnection(deviceOutputNodeResult.DeviceOutputNode);
+
+                result.Graph.Start();
             }
-
-            var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Audio/sent.mp3"));
-
-            var fileInputNodeResult = await result.Graph.CreateFileInputNodeAsync(file);
-            if (fileInputNodeResult.Status != AudioFileNodeCreationStatus.Success)
-            {
-                return;
-            }
-
-            var deviceOutputNodeResult = await result.Graph.CreateDeviceOutputNodeAsync();
-            if (deviceOutputNodeResult.Status != AudioDeviceNodeCreationStatus.Success)
-            {
-                return;
-            }
-
-            fileInputNodeResult.FileInputNode
-                .AddOutgoingConnection(deviceOutputNodeResult.DeviceOutputNode);
-
-            result.Graph.Start();
+            catch { }
         }
     }
 
