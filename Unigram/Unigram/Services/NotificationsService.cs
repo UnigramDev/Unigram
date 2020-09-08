903e66ff65a550bce62d0f9719898e594665fd96
Fela Ameghino <frayx@live.it>
2020-09-08 13:53:29 +0200

    SendMessageOptions => MessageSendOptions

--- Unigram/Unigram/Services/NotificationsService.cs
+++ Unigram/Unigram/Services/NotificationsService.cs
@@ -674,8 +674,8 @@ namespace Unigram.Services
                     var formatted = Client.Execute(new ParseMarkdown(new FormattedText(messageText, new TextEntity[0]))) as FormattedText;
 
                     var replyToMsgId = data.ContainsKey("msg_id") ? long.Parse(data["msg_id"]) << 20 : 0;
-                    var response = await _protoService.SendAsync(new SendMessage(chat.Id, replyToMsgId, new SendMessageOptions(false, true, null), null, new InputMessageText(formatted, false, false)));
-                
+                    var response = await _protoService.SendAsync(new SendMessage(chat.Id, replyToMsgId, new MessageSendOptions(false, true, null), null, new InputMessageText(formatted, false, false)));
+
                     if (chat.Type is ChatTypePrivate)
                     {
                         await _protoService.SendAsync(new ViewMessages(chat.Id, new long[] { chat.LastMessage.Id }, true));
