81e91552cf92d714e06841c78e3b3ffed32117e6
Fela Ameghino <frayx@live.it>
2020-09-23 10:20:52 +0200

    DateTimeParser

---
+++ Unigram/Unigram/Common/DateTimeParser.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Telegram.Td.Api;
+using Unigram.Services;
+
+namespace Unigram.Common
+{
+    public class DateTimeParser
+    {
+        private enum DateRangeFormat
+        {
+            Day,
+            Month,
+            Year
+        }
+
+        private readonly static Dictionary<string, DateRangeFormat> _formats = new Dictionary<string, DateRangeFormat>
+        {
+            { "dd MMM yyyy", DateRangeFormat.Day },
+            { "dd MMM", DateRangeFormat.Day },
+            { "MMM yyyy", DateRangeFormat.Month },
+            { "MMM", DateRangeFormat.Month },
+            { "yyyy", DateRangeFormat.Year }
+        };
+
+        public static IList<DateRange> Parse(string query)
+        {
+            var monthEngl = new string[12];
+            var monthInLocal = new string[12];
+
+            for (int i = 1; i <= 12; i++)
+            {
+                monthEngl[i - 1] = new CultureInfo("en").DateTimeFormat.GetMonthName(i).ToLower();
+                monthInLocal[i - 1] = new CultureInfo(LocaleService.Current.Language).DateTimeFormat.GetMonthName(i).ToLower();
+            }
+
+            var split = query.ToLower().Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                if (split[i].Length > 3)
+                {
+                    for (int j = 0; j < 12; j++)
+                    {
+                        if (monthEngl[j].StartsWith(split[i]) || monthInLocal[j].StartsWith(split[i]))
+                        {
+                            split[i] = split[i].Substring(0, 3);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            var text = string.Join(' ', split);
+            var results = new List<DateRange>();
+
+            foreach (var format in _formats)
+            {
+                if (TryParseExact(text, format.Key, out DateTime result))
+                {
+                    DateTime start = result.Date;
+                    DateTime end = result.Date;
+
+                    switch (format.Value)
+                    {
+                        case DateRangeFormat.Day:
+                            end = result.Date.AddDays(1);
+                            break;
+                        case DateRangeFormat.Month:
+                            end = result.Date.AddMonths(1);
+                            break;
+                        case DateRangeFormat.Year:
+                            end = result.Date.AddYears(1);
+                            break;
+                    }
+
+                    for (int i = start.Year; i >= 2010; i--)
+                    {
+                        results.Add(new DateRange
+                        {
+                            StartDate = start.AddYears(-(start.Year - i)).ToTimestamp(),
+                            EndDate = end.AddYears(-(start.Year - i)).ToTimestamp() - 1
+                        });
+                    }
+
+                    break;
+                }
+            }
+
+            return results;
+        }
+
+        static bool TryParseExact(string text, string format, out DateTime result)
+        {
+            if (DateTime.TryParseExact(text, format, new CultureInfo("en"), DateTimeStyles.None, out result))
+            {
+                return true;
+            }
+
+            return DateTime.TryParseExact(text, format, new CultureInfo("it"), DateTimeStyles.None, out result);
+        }
+    }
+}
