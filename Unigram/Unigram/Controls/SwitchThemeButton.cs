0e9115b62b71b3f7ab46a9eac345191c3caf380f
Fela Ameghino <frayx@live.it>
2020-09-23 10:20:33 +0200

    Added a switch

---
+++ Unigram/Unigram/Controls/SwitchThemeButton.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using Unigram.Assets.Animations;
+using Windows.UI.Xaml;
+
+namespace Unigram.Controls
+{
+    public class SwitchThemeButton : SimpleButton
+    {
+        private AnimatedVisualPlayer _player;
+
+        public SwitchThemeButton()
+        {
+            DefaultStyleKey = typeof(SwitchThemeButton);
+            ActualThemeChanged += OnActualThemeChanged;
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            _player = GetTemplateChild("ContentPresenter") as AnimatedVisualPlayer;
+            _player.Source = new SwitchThemeAnimation();
+            _player.SetProgress(ActualTheme == ElementTheme.Light ? 1 : 0);
+        }
+
+        private async void OnActualThemeChanged(FrameworkElement sender, object args)
+        {
+            _player.PlaybackRate = ActualTheme == ElementTheme.Light ? 1 : -1;
+            await _player.PlayAsync(0, 1, false);
+        }
+    }
+}
