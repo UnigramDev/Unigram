3170155d6ff67bb6f81862ad40b3d10b9e4773fb
Fela Ameghino <frayx@live.it>
2020-10-01 16:50:12 +0200

    Fixed sticker pack cell recycling crash

--- Unigram/Unigram/Views/Popups/StickerSetPopup.xaml.cs
+++ Unigram/Unigram/Views/Popups/StickerSetPopup.xaml.cs
@@ -168,7 +168,7 @@ namespace Unigram.Views.Popups
 
             if (args.Phase == 0)
             {
-                var title = content.Children[1] as TextBlock;
+                var title = content.Children[content.Children.Count - 1] as TextBlock;
                 title.Text = sticker.Emoji;
             }
             else if (args.Phase == 1)
