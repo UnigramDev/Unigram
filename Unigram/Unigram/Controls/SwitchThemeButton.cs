389a2038933a4ab78b34c8c89b9e19f1bbcc64e1
Fela Ameghino <frayx@live.it>
2020-10-06 20:16:43 +0200

    Fixed crash in switch theme button initialization

--- Unigram/Unigram/Controls/SwitchThemeButton.cs
+++ Unigram/Unigram/Controls/SwitchThemeButton.cs
@@ -19,15 +19,18 @@ namespace Unigram.Controls
             _player = GetTemplateChild("ContentPresenter") as LottieView;
             _player.IsLoopingEnabled = false;
             _player.IsCachingEnabled = false;
+            _player.IsBackward = ActualTheme == ElementTheme.Dark;
             _player.FrameSize = new SizeInt32 { Width = 40, Height = 40 };
             _player.Source = new Uri("ms-appx:///Assets/Animations/Sun.tgs");
-            _player.SetPosition(ActualTheme == ElementTheme.Dark ? 1 : 0);
         }
 
         private void OnActualThemeChanged(FrameworkElement sender, object args)
         {
-            _player.SetPosition(ActualTheme != ElementTheme.Dark ? 1 : 0);
-            _player.Play(ActualTheme != ElementTheme.Dark);
+            if (_player != null)
+            {
+                _player.SetPosition(ActualTheme != ElementTheme.Dark ? 1 : 0);
+                _player.Play(ActualTheme != ElementTheme.Dark);
+            }
         }
     }
 }
