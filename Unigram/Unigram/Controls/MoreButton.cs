9ac01905271a7364ea03260602de23f4a9160d61
Fela Ameghino <frayx@live.it>
2020-11-30 16:40:35 +0100

    Forgot

---
+++ Unigram/Unigram/Controls/MoreButton.cs
@@ -0,0 +1,10 @@
+﻿namespace Unigram.Controls
+{
+    public class MoreButton : GlyphButton
+    {
+        public MoreButton()
+        {
+            DefaultStyleKey = typeof(MoreButton);
+        }
+    }
+}
