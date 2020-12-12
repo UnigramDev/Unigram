117a088e3c72b42024c3f55cf985f5b7d43e65a1
Fela Ameghino <frayx@live.it>
2020-12-12 10:32:08 +0100

    CanCheckThreadAccess

--- Unigram/Unigram/Common/ApiInfo.cs
+++ Unigram/Unigram/Common/ApiInfo.cs
@@ -10,24 +10,27 @@ namespace Unigram.Common
     public static class ApiInfo
     {
         private static bool? _isStoreRelease;
-        public static bool IsStoreRelease => (_isStoreRelease = (_isStoreRelease ??= Package.Current.SignatureKind == PackageSignatureKind.Store)) ?? false;
+        public static bool IsStoreRelease => _isStoreRelease ??= (Package.Current.SignatureKind == PackageSignatureKind.Store);
 
         public static bool IsPackagedRelease => !IsStoreRelease;
 
         private static bool? _canUseDirectComposition;
-        public static bool CanUseDirectComposition => (_canUseDirectComposition = _canUseDirectComposition ?? ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 7)) ?? false;
+        public static bool CanUseDirectComposition => _canUseDirectComposition ??= ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 7);
 
         private static bool? _canUseViewports;
-        public static bool CanUseViewports => (_canUseViewports = _canUseViewports ?? ApiInformation.IsEventPresent("Windows.UI.Xaml.FrameworkElement", "EffectiveViewportChanged")) ?? false;
+        public static bool CanUseViewports => _canUseViewports ??= ApiInformation.IsEventPresent("Windows.UI.Xaml.FrameworkElement", "EffectiveViewportChanged");
 
         private static bool? _canUseWindowManagement;
-        public static bool CanUseWindowManagement => (_canUseWindowManagement = _canUseWindowManagement ?? ApiInformation.IsTypePresent("Windows.UI.WindowManagement.DisplayRegion")) ?? false;
+        public static bool CanUseWindowManagement => _canUseWindowManagement ??= ApiInformation.IsTypePresent("Windows.UI.WindowManagement.DisplayRegion");
 
         private static bool? _canUnconstrainFromBounds;
-        public static bool CanUnconstrainFromBounds => (_canUnconstrainFromBounds = _canUnconstrainFromBounds ?? ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.Primitives.Popup", "ShouldConstrainToRootBounds")) ?? false;
+        public static bool CanUnconstrainFromBounds => _canUnconstrainFromBounds ??= ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.Primitives.Popup", "ShouldConstrainToRootBounds");
+
+        private static bool? _canCheckThreadAccess;
+        public static bool CanCheckThreadAccess => _canCheckThreadAccess ??= ApiInformation.IsPropertyPresent("Windows.System.DispatcherQueue", "HasThreadAccess");
 
         private static bool? _isMediaSupported;
-        public static bool IsMediaSupported => (_isMediaSupported = _isMediaSupported ?? NativeUtils.IsMediaSupported()) ?? true;
+        public static bool IsMediaSupported => _isMediaSupported ??= NativeUtils.IsMediaSupported();
 
         private static ulong? _build;
         public static bool IsBuildOrGreater(ulong compare)
@@ -58,7 +61,7 @@ namespace Unigram.Common
 
 
         private static FlowDirection? _flowDirection;
-        public static FlowDirection FlowDirection => (_flowDirection = _flowDirection ?? LoadFlowDirection()) ?? FlowDirection.LeftToRight;
+        public static FlowDirection FlowDirection => _flowDirection ??= LoadFlowDirection();
 
         private static FlowDirection LoadFlowDirection()
         {
