22cc82046e90ce7a424a0d737b2246a62273866c
Fela Ameghino <frayx@live.it>
2021-03-29 10:03:44 +0200

    Fixed group call hangup state

--- Unigram/Unigram/Controls/GroupCallHeader.xaml.cs
+++ Unigram/Unigram/Controls/GroupCallHeader.xaml.cs
@@ -47,7 +47,7 @@ namespace Unigram.Controls
         {
             var visible = true;
 
-            if (chat.VoiceChat.GroupCallId == 0 || !chat.VoiceChat.HasParticipants || call == null || call.IsJoined)
+            if (chat.VoiceChat.GroupCallId != call?.Id || call == null || call.IsJoined || call.IsActive)
             {
                 ShowHide(false);
                 visible = false;
