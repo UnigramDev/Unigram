dff36c68ce3ad6b8a048bea4e793f1b927d74b26
Fela Ameghino <frayx@live.it>
2021-04-09 11:12:50 +0200

    Fix

--- Unigram/Unigram/Common/TdException.cs
+++ Unigram/Unigram/Common/TdException.cs
@@ -33,7 +33,7 @@ namespace Unigram
             return new TdException(message);
         }
 
-        private static bool IsDatabaseBrokenError(string message)
+        public static bool IsDatabaseBrokenError(string message)
         {
             return message.Contains("Wrong key or database is corrupted") ||
                     message.Contains("SQL logic error or missing database") ||
@@ -42,23 +42,19 @@ namespace Unigram
                     message.Contains("unsupported file format");
         }
 
-        private static bool IsDiskFullError(string message)
+        public static bool IsDiskFullError(string message)
         {
             return message.Contains("There is not enough space on the disk") ||
+                    message.Contains(": 112 :") ||
                     message.Contains("database or disk is full") ||
                     message.Contains("out of memory for database");
         }
 
-        private static bool IsDiskError(string message)
+        public static bool IsDiskError(string message)
         {
             // This is UNIX stuff and has no sense on Windows but I'm lazy to see
             // if there's any equivalents that we need to cover.
             return message.Contains("I/O error") || message.Contains("Structure needs cleaning");
-        }
-
-        private static bool IsExternalError(string message)
-        {
-            return IsDatabaseBrokenError(message) || IsDiskFullError(message) || IsDiskError(message);
         }
     }
 
