2bc40340c2643e7eb659d213c53d907714a0fcf4
Fela Ameghino <frayx@live.it>
2021-04-14 19:30:25 +0200

    Smaller switch theme button

--- Unigram/Unigram/Controls/SwitchThemeButton.cs
+++ Unigram/Unigram/Controls/SwitchThemeButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Windows.Graphics;
+using Windows.Graphics.Display;
 using Windows.UI.Xaml;
 
 namespace Unigram.Controls
@@ -16,11 +17,14 @@ namespace Unigram.Controls
 
         protected override void OnApplyTemplate()
         {
+            var dpi = DisplayInformation.GetForCurrentView().LogicalDpi / 96.0f;
+            var size = (int)(24 * dpi);
+
             _player = GetTemplateChild("ContentPresenter") as LottieView;
             _player.IsLoopingEnabled = false;
             _player.IsCachingEnabled = false;
             _player.IsBackward = ActualTheme == ElementTheme.Dark;
-            _player.FrameSize = new SizeInt32 { Width = 40, Height = 40 };
+            _player.FrameSize = new SizeInt32 { Width = size, Height = size };
             _player.Source = new Uri("ms-appx:///Assets/Animations/Sun.tgs");
         }
 
