b4f85a4f4b81155e69779d59170d5db7e0f837af
Fela Ameghino <frayx@live.it>
2021-05-11 21:48:56 +0200

    RectangleClip interop

---
+++ Unigram/Unigram.Native/Composition.CompositionDevice.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "Composition.CompositionDevice.g.h"
+
+#include <winrt/windows.ui.composition.h>
+
+using namespace winrt::Windows::UI::Composition;
+
+namespace winrt::Unigram::Native::Composition::implementation
+{
+    static struct CompositionDevice : CompositionDeviceT<CompositionDevice>
+    {
+        static winrt::Unigram::Native::Composition::DirectRectangleClip CreateRectangleClip(Visual visual);
+    };
+}
+
+namespace winrt::Unigram::Native::Composition::factory_implementation
+{
+    struct CompositionDevice : CompositionDeviceT<CompositionDevice, implementation::CompositionDevice>
+    {
+    };
+}
