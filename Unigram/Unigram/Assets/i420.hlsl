f33894ab37c3d2415db370cda03bb15abd123900
Fela Ameghino <frayx@live.it>
2021-06-04 15:56:08 +0200

    Improved video chat preview quality

--- Unigram/Unigram/Assets/i420.hlsl
+++ Unigram/Unigram/Assets/i420.hlsl
@@ -11,11 +11,14 @@ D2D_PS_ENTRY(main)
 {
     float4 position = D2DGetScenePosition();
 
-    float y = D2DGetInput(0).x;
-    float u = D2DSampleInputAtPosition(1, position.xy / 2).x - 0.5;
-    float v = D2DSampleInputAtPosition(2, position.xy / 2).x - 0.5;
+    float y = D2DGetInput(0).x - 0.0625;
+    float u = D2DSampleInputAtPosition(1, floor(position.xy / 2)).x - 0.5;
+    float v = D2DSampleInputAtPosition(2, floor(position.xy / 2)).x - 0.5;
     float a = D2DGetInput(3).a;
 
-    // TODO: Explicit reference to `a` is needed because of optimizations.
-    return float4(y + 1.403 * v, y - 0.344 * u - 0.714 * v, y + 1.77 * u, a);
+    // TODO: Explicit reference to `a` is needed because of compiler optimizations.
+    return float4(1.164 * y + 1.596 * v,
+        1.164 * y - 0.392 * u - 0.813 * v,
+        1.164 * y + 2.17 * u,
+        a);
 }
