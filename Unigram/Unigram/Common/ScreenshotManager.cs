e18af4287e48386fc4d807837f7430d5c8c73dd2
Fela Ameghino <frayx@live.it>
2021-12-09 23:01:26 +0100

    ExactSpelling

--- Unigram/Unigram/Common/ScreenshotManager.cs
+++ Unigram/Unigram/Common/ScreenshotManager.cs
@@ -121,31 +121,31 @@ namespace Unigram.Common
             public uint[] cols;
         }
 
-        [DllImport("gdi32.dll", SetLastError = true)]
+        [DllImport("gdi32.dll", ExactSpelling = true, SetLastError = true)]
         static extern IntPtr CreateCompatibleDC(IntPtr hdc);
 
-        [DllImport("gdi32.dll", EntryPoint = "CreateCompatibleBitmap")]
+        [DllImport("gdi32.dll", EntryPoint = "CreateCompatibleBitmap", ExactSpelling = true)]
         static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
 
-        [DllImport("gdi32.dll", EntryPoint = "SelectObject")]
+        [DllImport("gdi32.dll", EntryPoint = "SelectObject", ExactSpelling = true)]
         public static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
 
-        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject", ExactSpelling = true)]
         public static extern bool DeleteObject(IntPtr hObject);
 
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", ExactSpelling = true)]
         static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
 
-        [DllImport("user32.dll", SetLastError = true)]
+        [DllImport("user32.dll", ExactSpelling = true, SetLastError = true)]
         static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
 
-        [DllImport("user32.dll", SetLastError = true)]
+        [DllImport("user32.dll", ExactSpelling = true, SetLastError = true)]
         static extern IntPtr GetDC(IntPtr hWnd);
 
-        [DllImport("user32.dll", SetLastError = true)]
+        [DllImport("user32.dll", ExactSpelling = true, SetLastError = true)]
         static extern bool PrintWindow(IntPtr hwnd, IntPtr hDC, uint nFlags);
 
-        [DllImport("gdi32.dll", EntryPoint = "GetDIBits")]
+        [DllImport("gdi32.dll", EntryPoint = "GetDIBits", ExactSpelling = true)]
         static extern int GetDIBits(IntPtr hdc, IntPtr hbmp, uint uStartScan, uint cScanLines, [Out] byte[] lpvBits, ref BITMAPINFO lpbi, DIB_Color_Mode uUsage);
 
         #endregion
