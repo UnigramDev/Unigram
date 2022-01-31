feedfecbdcc9bbf9cbf604f6f9cdf03385313f36
Fela Ameghino <frayx@live.it>
2022-01-31 21:05:17 +0100

    Typos

--- Unigram/Unigram/Services/SettingsService.cs
+++ Unigram/Unigram/Services/SettingsService.cs
@@ -217,21 +217,21 @@ namespace Unigram.Services
 
         #region App version
 
-        public const ulong CurrentVersion = (8UL << 48) | (4UL << 32) | (0UL << 16);
+        public const ulong CurrentVersion = (8UL << 48) | (5UL << 32) | (0UL << 16);
         public const string CurrentChangelog = @"Video Stickers, Better Reactions and More
 
-Video Stickers
+**Video Stickers**
 • Use a new type of detailed stickers with smooth animations.
 • Create new sets by sending .webm videos to @stickers.
 • Bring your custom animated stickers from other apps.
 
-Reactions
+**Reactions**
 • Right click a message for more reactions. 
 • Group and Channel admins can enable reactions in their chat via Chat Info > Edit > Reactions.
 • See real-time animations in chat when a user reacts to your message.
 • React with additional emoji: 🥰🤯🤔🤬👏. 
 
-Read Status for Reactions
+**Read Status for Reactions**
 • Click the new button in chats to jump to your messages that have unseen reactions.
 • Watch the animations for unseen reactions play when you hit the button.";
 
