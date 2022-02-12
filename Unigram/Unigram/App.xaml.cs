74783ad73a28f8e69e70a2f8c00359b15bfd4700
Fela Ameghino <frayx@live.it>
2022-02-12 23:09:48 +0100

    Try to workaround ToolTip's NotSupportedException

--- Unigram/Unigram/App.xaml.cs
+++ Unigram/Unigram/App.xaml.cs
@@ -74,24 +74,13 @@ namespace Unigram
             UnhandledException += (s, args) =>
             {
                 args.Handled = true;
-
-                if (args.Exception is not NotSupportedException)
-                {
-                    Client.Execute(new AddLogMessage(1, "Unhandled exception:\n" + args.Exception.ToString()));
-                }
+                Client.Execute(new AddLogMessage(1, "Unhandled exception:\n" + args.Exception.ToString()));
             };
 
 #if !DEBUG
             Microsoft.AppCenter.AppCenter.Start(Constants.AppCenterId,
                 typeof(Microsoft.AppCenter.Analytics.Analytics),
                 typeof(Microsoft.AppCenter.Crashes.Crashes));
-
-            Microsoft.AppCenter.Crashes.Crashes.ShouldProcessErrorReport = error =>
-            {
-#pragma warning disable CS0618 // Type or member is obsolete
-                return error.Exception is not NotSupportedException;
-#pragma warning restore CS0618 // Type or member is obsolete
-            };
 
             Microsoft.AppCenter.Analytics.Analytics.TrackEvent("Windows",
                 new System.Collections.Generic.Dictionary<string, string>
