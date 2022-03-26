8cb30472672dd4c5891af5623d7f5cf808216ddc
Fela Ameghino <frayx@live.it>
2022-03-26 15:22:09 +0100

    Animated more

--- Unigram/Unigram/Controls/MoreButton.cs
+++ Unigram/Unigram/Controls/MoreButton.cs
@@ -1,10 +1,31 @@
-﻿namespace Unigram.Controls
+﻿using Unigram.Assets.Icons;
+using Windows.UI;
+using Windows.UI.Xaml;
+
+namespace Unigram.Controls
 {
-    public class MoreButton : GlyphButton
+    public class MoreButton : BadgeButton
     {
         public MoreButton()
         {
             DefaultStyleKey = typeof(MoreButton);
+            IconSource = new More();
+
+            ActualThemeChanged += OnActualThemeChanged;
+            ThemeChanged();
+        }
+
+        private void OnActualThemeChanged(FrameworkElement sender, object args)
+        {
+            ThemeChanged();
+        }
+
+        private void ThemeChanged()
+        {
+            if (IconSource != null)
+            {
+                IconSource.SetColorProperty("Foreground", ActualTheme == ElementTheme.Light ? Colors.Black : Colors.White);
+            }
         }
     }
 }
