eb53f2a046e915dbe534f404fc6f22357bd28275
Fela Ameghino <frayx@live.it>
2022-05-07 17:05:00 +0200

    Use new session types

--- Unigram/Unigram/Controls/Cells/SessionCell.xaml.cs
+++ Unigram/Unigram/Controls/Cells/SessionCell.xaml.cs
@@ -41,72 +41,24 @@ namespace Unigram.Controls.Cells
 
         public static (string Glyph, Color Backgroud, string Animation) IconForSession(Session session)
         {
-            var platform = session.Platform.ToLowerInvariant();
-            var device = session.DeviceModel.ToLowerInvariant();
-            var systemVersion = session.SystemVersion.ToLowerInvariant();
-
-            if (device.Contains("xbox"))
+            return session.Type switch
             {
-                return ("", Color.FromArgb(0xff, 0x35, 0xc7, 0x59), null);
-            }
-            if (device.Contains("chrome") && !device.Contains("chromebook"))
-            {
-                return ("\uE96D", Color.FromArgb(0xFF, 0x35, 0xC7, 0x59), "Chrome");
-            }
-            if (device.Contains("brave"))
-            {
-                return ("", Color.FromArgb(0xFF, 0xFF, 0x95, 0x00), null);
-            }
-            if (device.Contains("vivaldi"))
-            {
-                return ("", Color.FromArgb(0xFF, 0xFF, 0x3C, 0x30), null);
-            }
-            if (device.Contains("safari"))
-            {
-                return ("\uE974", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Safari");
-            }
-            if (device.Contains("firefox"))
-            {
-                return ("\uE96F", Color.FromArgb(0xFF, 0xFF, 0x95, 0x00), "Firefox");
-            }
-            if (device.Contains("opera"))
-            {
-                return ("", Color.FromArgb(0xFF, 0xFF, 0x3C, 0x30), null);
-            }
-            if (platform.Contains("android"))
-            {
-                return ("\uE96C", Color.FromArgb(0xFF, 0x35, 0xC7, 0x59), "Android");
-            }
-            if (device.Contains("iphone"))
-            {
-                return ("\uE971", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Iphone");
-            }
-            if (device.Contains("ipad"))
-            {
-                return ("\uE970", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Ipad");
-            }
-            if ((platform.Contains("macos") || systemVersion.Contains("macos")) && device.Contains("mac"))
-            {
-                return ("\uE973", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Mac");
-            }
-            if (platform.Contains("ios") || platform.Contains("macos") || systemVersion.Contains("macos"))
-            {
-                return ("\uE971", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Iphone");
-            }
-            if (platform.Contains("ubuntu") || systemVersion.Contains("ubuntu"))
-            {
-                return ("\uE976", Color.FromArgb(0xFF, 0xFF, 0x95, 0x00), "Ubuntu");
-            }
-            if (platform.Contains("linux") || systemVersion.Contains("linux"))
-            {
-                return ("\uE972", Color.FromArgb(0xFF, 0x8E, 0x8E, 0x93), "Linux");
-            }
-            if (platform.Contains("windows") || systemVersion.Contains("windows"))
-            {
-                return ("\uE977", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Windows");
-            }
-
-            return ("", Color.FromArgb(0xFF, 0x8E, 0x8E, 0x93), null);
+                SessionTypeXbox => ("", Color.FromArgb(0xff, 0x35, 0xc7, 0x59), null),
+                SessionTypeChrome => ("\uE96D", Color.FromArgb(0xFF, 0x35, 0xC7, 0x59), "Chrome"),
+                SessionTypeBrave => ("", Color.FromArgb(0xFF, 0xFF, 0x95, 0x00), null),
+                SessionTypeVivaldi => ("", Color.FromArgb(0xFF, 0xFF, 0x3C, 0x30), null),
+                SessionTypeSafari => ("\uE974", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Safari"),
+                SessionTypeFirefox => ("\uE96F", Color.FromArgb(0xFF, 0xFF, 0x95, 0x00), "Firefox"),
+                SessionTypeOpera => ("", Color.FromArgb(0xFF, 0xFF, 0x3C, 0x30), null),
+                SessionTypeAndroid => ("\uE96C", Color.FromArgb(0xFF, 0x35, 0xC7, 0x59), "Android"),
+                SessionTypeIphone => ("\uE971", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Iphone"),
+                SessionTypeIpad => ("\uE970", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Ipad"),
+                SessionTypeMac => ("\uE973", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Mac"),
+                SessionTypeUbuntu => ("\uE976", Color.FromArgb(0xFF, 0xFF, 0x95, 0x00), "Ubuntu"),
+                SessionTypeLinux => ("\uE972", Color.FromArgb(0xFF, 0x8E, 0x8E, 0x93), "Linux"),
+                SessionTypeWindows => ("\uE977", Color.FromArgb(0xFF, 0x00, 0x79, 0xFF), "Windows"),
+                _ => ("", Color.FromArgb(0xFF, 0x8E, 0x8E, 0x93), null)
+            };
         }
     }
 }
