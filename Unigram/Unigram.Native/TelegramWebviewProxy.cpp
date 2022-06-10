aec7c9beb98bc98f014de1758f26c4966b33d3c0
Fela Ameghino <frayx@live.it>
2022-06-10 18:43:54 +0200

    Webview proxy

---
+++ Unigram/Unigram.Native/TelegramWebviewProxy.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "TelegramWebviewProxy.h"
+#if __has_include("TelegramWebviewProxy.g.cpp")
+#include "TelegramWebviewProxy.g.cpp"
+#endif
+
+#include <winrt/Windows.Data.Json.h>
+
+using namespace winrt::Windows::Data::Json;
+
+namespace winrt::Unigram::Native::implementation
+{
+	TelegramWebviewProxy::TelegramWebviewProxy(TelegramWebviewProxyDelegate delegate)
+	{
+		_delegate = delegate;
+	}
+
+	void TelegramWebviewProxy::PostEvent(hstring eventName, hstring eventData)
+	{
+		_delegate(eventName, eventData);
+		return;
+
+		if (eventName == L"payment_form_submit")
+		{
+			try
+			{
+				auto json = JsonObject::Parse(eventData);
+				auto response = json.GetNamedValue(L"credentials");
+				auto title = json.GetNamedString(L"title", L"");
+
+				_delegate(title, response.Stringify());
+			}
+			catch (winrt::hresult_error const& ex)
+			{
+				_delegate(L"", eventData);
+			}
+		}
+	}
+} // namespace winrt::Unigram::Native::implementation
---
+++ Unigram/Unigram.Native/TelegramWebviewProxy.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "TelegramWebviewProxy.g.h"
+
+namespace winrt::Unigram::Native::implementation
+{
+	struct TelegramWebviewProxy : TelegramWebviewProxyT<TelegramWebviewProxy>
+	{
+	public:
+		TelegramWebviewProxy(TelegramWebviewProxyDelegate delegate);
+
+		void PostEvent(hstring eventName, hstring eventData);
+
+	private:
+		TelegramWebviewProxyDelegate _delegate;
+	};
+} // namespace winrt::Unigram::Native::implementation
+
+namespace winrt::Unigram::Native::factory_implementation
+{
+	struct TelegramWebviewProxy : TelegramWebviewProxyT<TelegramWebviewProxy, implementation::TelegramWebviewProxy>
+	{
+	};
+} // namespace winrt::Unigram::Native::factory_implementation
