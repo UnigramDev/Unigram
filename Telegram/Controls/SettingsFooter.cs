057c524fe24a716b1ec793f9b748480316e7e39d
Fela Ameghino <frayx@live.it>
2023-04-24 15:29:02 +0400

    Added SettingsFooter control

---
+++ Telegram/Controls/SettingsFooter.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Telegram.Controls
+{
+    public class SettingsFooter : Control
+    {
+        public SettingsFooter()
+        {
+            DefaultStyleKey = typeof(SettingsFooter);
+        }
+
+        #region Text
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(SettingsFooter), new PropertyMetadata(string.Empty));
+
+        #endregion
+    }
+}
