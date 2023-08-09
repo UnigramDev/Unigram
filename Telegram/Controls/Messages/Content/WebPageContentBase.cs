8e21d81f686a52d6399bf512904a7ff682fe4afb
Fela <frayx@live.it>
2023-08-09 14:28:11 +0200

    Show "Request to join" in link previews

--- Telegram/Controls/Messages/Content/WebPageContentBase.cs
+++ Telegram/Controls/Messages/Content/WebPageContentBase.cs
@@ -130,6 +130,17 @@ namespace Telegram.Controls.Messages.Content
 
                 button.Visibility = Visibility.Visible;
             }
+            else if (string.Equals(webPage.Type, "telegram_channel_request", StringComparison.OrdinalIgnoreCase))
+            {
+                if (run1 != null)
+                {
+                    run1.Text = run3.Text = string.Empty;
+                    run2.Text = Strings.RequestToJoin;
+                    run3.Foreground = null;
+                }
+
+                button.Visibility = Visibility.Visible;
+            }
             else if (string.Equals(webPage.Type, "telegram_message", StringComparison.OrdinalIgnoreCase))
             {
                 if (run1 != null)
