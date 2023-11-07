95557fcddd4d533466ff0968ff8291907434d485
Fela <frayx@live.it>
2023-11-07 16:44:39 +0400

    Badge button skeleton

--- Telegram/Controls/BadgeButton.cs
+++ Telegram/Controls/BadgeButton.cs
@@ -5,11 +5,16 @@
 // file LICENSE or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
 //
 using Microsoft.UI.Xaml.Controls;
+using System;
 using System.Collections.Generic;
+using System.Numerics;
+using Windows.UI;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Automation;
 using Windows.UI.Xaml.Automation.Peers;
 using Windows.UI.Xaml.Automation.Provider;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Media;
 
 namespace Telegram.Controls
@@ -158,6 +163,62 @@ namespace Telegram.Controls
         {
             return _peer ??= new BadgeButtonAutomationPeer(this);
         }
+
+        #region Skeleton
+
+        public void ShowSkeleton()
+        {
+            if (ActualSize.X == 0 || ActualSize.Y == 0)
+            {
+                return;
+            }
+
+            var compositor = Window.Current.Compositor;
+            var rectangle = compositor.CreateRoundedRectangleGeometry();
+            rectangle.Size = new Vector2(ActualSize.X - 2, ActualSize.Y - 2);
+            rectangle.Offset = new Vector2(1, 1);
+            rectangle.CornerRadius = new Vector2(4);
+
+            var strokeColor = Background is SolidColorBrush brush ? brush.Color : Colors.White;
+
+            var stroke = compositor.CreateLinearGradientBrush();
+            stroke.ColorStops.Add(compositor.CreateColorGradientStop(0.0f, Color.FromArgb(0x00, strokeColor.R, strokeColor.G, strokeColor.B)));
+            stroke.ColorStops.Add(compositor.CreateColorGradientStop(0.5f, Color.FromArgb(0xaa, strokeColor.R, strokeColor.G, strokeColor.B)));
+            stroke.ColorStops.Add(compositor.CreateColorGradientStop(1.0f, Color.FromArgb(0x00, strokeColor.R, strokeColor.G, strokeColor.B)));
+
+            var fill = compositor.CreateLinearGradientBrush();
+            fill.ColorStops.Add(compositor.CreateColorGradientStop(0.0f, Color.FromArgb(0x00, 0xff, 0xff, 0xff)));
+            fill.ColorStops.Add(compositor.CreateColorGradientStop(0.5f, Color.FromArgb(0xaa, 0xff, 0xff, 0xff)));
+            fill.ColorStops.Add(compositor.CreateColorGradientStop(1.0f, Color.FromArgb(0x00, 0xff, 0xff, 0xff)));
+
+            var shape = compositor.CreateSpriteShape();
+            shape.Geometry = rectangle;
+            shape.FillBrush = fill;
+            shape.StrokeBrush = stroke;
+            shape.StrokeThickness = 1;
+
+            var visual = compositor.CreateShapeVisual();
+            visual.Size = new Vector2(ActualSize.X, ActualSize.Y);
+            visual.Shapes.Add(shape);
+
+            var endless = compositor.CreateScalarKeyFrameAnimation();
+            endless.InsertKeyFrame(0, -ActualSize.X);
+            endless.InsertKeyFrame(1, +ActualSize.X);
+            endless.IterationBehavior = AnimationIterationBehavior.Forever;
+            endless.Duration = TimeSpan.FromMilliseconds(2000);
+
+            stroke.StartAnimation("Offset.X", endless);
+            fill.StartAnimation("Offset.X", endless);
+
+            ElementCompositionPreview.SetElementChildVisual(this, visual);
+        }
+
+        public void HideSkeleton()
+        {
+            ElementCompositionPreview.SetElementChildVisual(this, Window.Current.Compositor.CreateSpriteVisual());
+        }
+
+        #endregion
     }
 
     public class BadgeButtonWithImage : BadgeButton
