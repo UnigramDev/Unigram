09a276966ec3880f0e11e78c33778e6a43939c63
Fela <frayx@live.it>
2023-12-18 19:08:08 +0400

    Removed old sticker set template

--- Telegram/Views/Supergroups/SupergroupEditStickerSetPage.xaml.cs
+++ Telegram/Views/Supergroups/SupergroupEditStickerSetPage.xaml.cs
@@ -6,6 +6,8 @@
 //
 using System.Linq;
 using Telegram.Common;
+using Telegram.Controls;
+using Telegram.Streams;
 using Telegram.Td.Api;
 using Telegram.ViewModels.Supergroups;
 using Windows.UI.Xaml;
@@ -17,10 +19,14 @@ namespace Telegram.Views.Supergroups
     {
         public SupergroupEditStickerSetViewModel ViewModel => DataContext as SupergroupEditStickerSetViewModel;
 
+        private readonly AnimatedListHandler _handler;
+
         public SupergroupEditStickerSetPage()
         {
             InitializeComponent();
             Title = Strings.GroupStickers;
+
+            _handler = new AnimatedListHandler(ScrollingHost, AnimatedListType.Stickers);
 
             var debouncer = new EventDebouncer<TextChangedEventArgs>(Constants.TypingTimeout, handler => ShortName.TextChanged += new TextChangedEventHandler(handler));
             debouncer.Invoked += (s, args) =>
@@ -41,49 +47,20 @@ namespace Telegram.Views.Supergroups
             var content = args.ItemContainer.ContentTemplateRoot as Grid;
             var stickerSet = args.Item as StickerSetInfo;
 
-            if (args.Phase == 0)
-            {
-                var title = content.Children[1] as TextBlock;
-                title.Text = stickerSet.Title;
-            }
-            else if (args.Phase == 1)
-            {
-                var subtitle = content.Children[2] as TextBlock;
-                subtitle.Text = Locale.Declension(Strings.R.Stickers, stickerSet.Size);
-            }
-            else if (args.Phase == 2)
-            {
-                var photo = content.Children[0] as Image;
+            var title = content.Children[1] as TextBlock;
+            title.Text = stickerSet.Title;
 
-                var cover = stickerSet.Thumbnail ?? stickerSet.Covers.FirstOrDefault()?.Thumbnail;
-                if (cover == null)
-                {
-                    return;
-                }
+            var subtitle = content.Children[2] as TextBlock;
+            subtitle.Text = Locale.Declension(Strings.R.Stickers, stickerSet.Size);
 
-                var file = cover.File;
-                if (file.Local.IsDownloadingCompleted)
-                {
-                    if (cover.Format is ThumbnailFormatTgs)
-                    {
-                        photo.Source = PlaceholderHelper.GetLottieFrame(file.Local.Path, 0, 48, 48);
-                    }
-                    else
-                    {
-                        photo.Source = PlaceholderHelper.GetWebPFrame(file.Local.Path, 48);
-                    }
-                }
-                else if (file.Local.CanBeDownloaded && !file.Local.IsDownloadingActive)
-                {
-                    photo.Source = null;
-                    ViewModel.ClientService.DownloadFile(file.Id, 1);
-                }
+            var cover = stickerSet.GetThumbnail();
+            if (cover == null)
+            {
+                return;
             }
 
-            if (args.Phase < 2)
-            {
-                args.RegisterUpdateCallback(OnContainerContentChanging);
-            }
+            var animated = content.Children[0] as AnimatedImage;
+            animated.Source = new DelayedFileSource(ViewModel.ClientService, cover);
 
             args.Handled = true;
         }
