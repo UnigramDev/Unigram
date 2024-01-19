1869df06784c4f800323dfad95419c62e5707cca
Fela <frayx@live.it>
2024-01-19 17:40:59 +0400

    Try to save some ram on Xbox by not caching chat page

--- Telegram/Views/ChatPage.xaml.cs
+++ Telegram/Views/ChatPage.xaml.cs
@@ -4,6 +4,7 @@
 // Distributed under the GNU General Public License v3.0. (See accompanying
 // file LICENSE or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
 //
+using Telegram.Common;
 using Telegram.Navigation;
 using Telegram.ViewModels;
 using Telegram.ViewModels.Delegates;
@@ -18,7 +19,9 @@ namespace Telegram.Views
         public ChatPage()
         {
             InitializeComponent();
-            NavigationCacheMode = Constants.DEBUG
+            NavigationCacheMode = ApiInfo.IsXbox
+                ? NavigationCacheMode.Disabled
+                : Constants.DEBUG
                 ? NavigationCacheMode.Disabled
                 : NavigationCacheMode.Required;
         }
