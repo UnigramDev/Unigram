02e2edb56cc0cb92e3546b72e7e9e70c1b151519
Fela <frayx@live.it>
2024-08-28 13:12:15 +0400

    Patch

--- Telegram/Rg.DiffUtils/DiffObservableCollection.cs
+++ Telegram/Rg.DiffUtils/DiffObservableCollection.cs
@@ -4,7 +4,7 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
-using Telegram.Collections;
+using Telegram;
 using WinRT;
 
 namespace Rg.DiffUtils
