ff00851d23be4792f17f8220bf5be4ef221445a2
Fela <frayx@live.it>
2024-09-05 14:20:30 +0400

    Fix channel footer

--- Telegram/Controls/Messages/MessageFooter.xaml.cs
+++ Telegram/Controls/Messages/MessageFooter.xaml.cs
@@ -333,7 +333,7 @@ namespace Telegram.Controls.Messages
                 _repliesLabel = string.Empty;
             }
 
-            if (message.IsChannelPost && message.SenderId is MessageSenderChat && !string.IsNullOrEmpty(message.AuthorSignature))
+            if (message.IsChannelPost && message.SenderId.IsChat(message.ChatId) && !string.IsNullOrEmpty(message.AuthorSignature))
             {
                 _authorLabel = $"{message.AuthorSignature}, ";
             }
