10bd0ac21d86027790436b547af79bce6d976787
Fela <frayx@live.it>
2024-10-09 17:41:59 +0400

    Improve slider state

--- Telegram/Common/SliderHelper.cs
+++ Telegram/Common/SliderHelper.cs
@@ -10,30 +10,41 @@ namespace Telegram.Common
     {
         public static void InitializeTicks(Slider slider, Grid container, int count, Func<int, string> callback)
         {
-            int j = 0;
+            for (int i = 0; i < container.Children.Count - 1; i++)
+            {
+                container.Children.RemoveAt(i);
+            }
+
+            container.ColumnDefinitions.Clear();
+            container.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(12, GridUnitType.Pixel) });
+
+            int j = 1;
             for (int i = 0; i < count; i++)
             {
                 var label = new TextBlock
                 {
                     Text = callback(i),
-                    TextAlignment = TextAlignment.Center,
+                    TextAlignment = i == 0 ? TextAlignment.Left : i == count - 1 ? TextAlignment.Right : TextAlignment.Center,
                     HorizontalAlignment = HorizontalAlignment.Stretch,
                     Style = BootStrapper.Current.Resources["InfoCaptionTextBlockStyle"] as Style,
                     FontFamily = BootStrapper.Current.Resources["EmojiThemeFontFamilyWithSymbols"] as FontFamily
                 };
 
-                Grid.SetColumn(label, j);
-
-                container.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) });
-
-                if (i < count - 1)
+                if (i > 0 && i < count - 1)
                 {
                     container.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                    Grid.SetColumn(label, ++j);
+                }
+                else
+                {
+                    container.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.5, GridUnitType.Star) });
+                    Grid.SetColumnSpan(label, count + 2);
                 }
 
                 container.Children.Add(label);
-                j += 2;
             }
+
+            container.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(12, GridUnitType.Pixel) });
 
             Grid.SetColumnSpan(slider, container.ColumnDefinitions.Count);
         }
