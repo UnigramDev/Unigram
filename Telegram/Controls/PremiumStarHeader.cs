033e10353abc10e76598354562e1eae24a434e6a
Fela <frayx@live.it>
2024-10-15 13:50:28 +0400

    Missing text, stars background

---
+++ Telegram/Controls/PremiumStarHeader.cs
@@ -0,0 +1,18 @@
+﻿//
+// Copyright Fela Ameghino 2015-2024
+//
+// Distributed under the GNU General Public License v3.0. (See accompanying
+// file LICENSE or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
+//
+using Windows.UI.Xaml.Controls;
+
+namespace Telegram.Controls
+{
+    public partial class PremiumStarHeader : Control
+    {
+        public PremiumStarHeader()
+        {
+            DefaultStyleKey = typeof(PremiumStarHeader);
+        }
+    }
+}
