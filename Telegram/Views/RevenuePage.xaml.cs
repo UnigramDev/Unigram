9435982c0dbef6e1e61dd6828405b84bd3dd75a3
Fela <frayx@live.it>
2026-09-17 20:06:26 +0400

    Use new sticky header in revenue screen

--- Telegram/Views/RevenuePage.xaml.cs
+++ Telegram/Views/RevenuePage.xaml.cs
@@ -5,7 +5,6 @@
 // file LICENSE or copy at https://www.gnu.org/licenses/gpl-3.0.txt)
 //
 
-using System;
 using System.ComponentModel;
 using Telegram.Common;
 using Telegram.Navigation;
@@ -14,9 +13,7 @@ using Telegram.ViewModels;
 using Telegram.Views.Chats;
 using Telegram.Views.Profile;
 using Windows.UI.Composition;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
@@ -33,46 +30,8 @@ namespace Telegram.Views
         public RevenuePage()
         {
             InitializeComponent();
-            InitializeScrolling();
 
             Title = Strings.Monetization;
-
-            _loader = new ScrollViewerIncrementalLoader(ScrollingHost);
-        }
-
-        private void InitializeScrolling()
-        {
-            var properties = ElementCompositionPreview.GetScrollViewerManipulationPropertySet(ScrollingHost);
-            var visual = ElementComposition.GetElementVisual(HeaderPanel);
-            var border = ElementComposition.GetElementVisual(CardBackground);
-            var clipper = ElementComposition.GetElementVisual(ClipperBackground);
-
-            ElementCompositionPreview.SetIsTranslationEnabled(HeaderPanel, true);
-
-            ProfileHeader.Height = 48 - 16;
-
-            _properties = visual.Compositor.CreatePropertySet();
-            _properties.InsertScalar("ActualHeight", ProfileHeader.ActualSize.Y + 16);
-
-            var translation = visual.Compositor.CreateExpressionAnimation(
-                "properties.ActualHeight > 16 ? scrollViewer.Translation.Y > -properties.ActualHeight ? 0 : -scrollViewer.Translation.Y - properties.ActualHeight : -scrollViewer.Translation.Y");
-            translation.SetReferenceParameter("scrollViewer", properties);
-            translation.SetReferenceParameter("properties", _properties);
-
-            var fadeOut = visual.Compositor.CreateExpressionAnimation(
-                "properties.ActualHeight > 16 ? scrollViewer.Translation.Y > -(properties.ActualHeight - 16) ? 1 : 1 - ((-scrollViewer.Translation.Y - (properties.ActualHeight - 16)) / 16) : 0");
-            fadeOut.SetReferenceParameter("scrollViewer", properties);
-            fadeOut.SetReferenceParameter("properties", _properties);
-
-            var fadeIn = visual.Compositor.CreateExpressionAnimation(
-                "properties.ActualHeight > 16 ? scrollViewer.Translation.Y > -(properties.ActualHeight - 16) ? 0 : ((-scrollViewer.Translation.Y - (properties.ActualHeight - 16)) / 16) : 1");
-            fadeIn.SetReferenceParameter("scrollViewer", properties);
-            fadeIn.SetReferenceParameter("properties", _properties);
-
-            visual.StartAnimation("Translation.Y", translation);
-
-            border.StartAnimation("Opacity", fadeOut);
-            clipper.StartAnimation("Opacity", fadeIn);
         }
 
         public void OnBackRequested(BackRequestedRoutedEventArgs args)
@@ -111,18 +70,8 @@ namespace Telegram.Views
             }
         }
 
-        private long _itemsSourceToken;
-
         private void OnNavigating(object sender, NavigatingCancelEventArgs e)
         {
-            // Ahead of the page that is leaving nulling its own source: the callback has to be
-            // gone before that, or the teardown arrives as a change.
-            _loader.ItemsSource = null;
-
-            if (TryGetScrollingHost(MediaFrame.Content, out ListViewBase scrollingHost))
-            {
-                scrollingHost.UnregisterPropertyChangedCallback(ItemsControl.ItemsSourceProperty, ref _itemsSourceToken);
-            }
         }
 
         private void OnNavigated(object sender, NavigationEventArgs e)
@@ -139,54 +88,28 @@ namespace Telegram.Views
             {
                 revenue.DataContext = ViewModel.Revenue;
             }
+        }
 
-            if (!TryGetScrollingHost(e.Content, out ListViewBase scrollingHost))
+        private int _prevSelectedIndex = -1;
+
+        private void Navigation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Navigation.SelectedItem is RevenueTabItem page &&page.Type != MediaFrame.Content?.GetType())
             {
-                return;
+                Logger.Info(page.Type);
+
+                NavigationTransitionInfo transition = _prevSelectedIndex == -1
+                    ? new SuppressNavigationTransitionInfo()
+                    : new SlideNavigationTransitionInfo
+                    {
+                        Effect = _prevSelectedIndex < Navigation.SelectedIndex
+                            ? SlideNavigationTransitionEffect.FromRight
+                            : SlideNavigationTransitionEffect.FromLeft
+                    };
+
+                _prevSelectedIndex = Navigation.SelectedIndex;
+                MediaFrame.Navigate(page.Type, null, transition);
             }
-
-            _loader.ItemsSource = scrollingHost.ItemsSource;
-            scrollingHost.RegisterPropertyChangedCallback(ItemsControl.ItemsSourceProperty, OnItemsSourceChanged, ref _itemsSourceToken);
-        }
-
-        private void OnItemsSourceChanged(DependencyObject sender, DependencyProperty dp)
-        {
-            if (sender is ListViewBase scrollingHost)
-            {
-                _loader.ItemsSource = scrollingHost.ItemsSource;
-            }
-        }
-
-        private bool TryGetScrollingHost(object content, out ListViewBase scrollingHost)
-        {
-            if (content is Page page)
-            {
-                scrollingHost = page.FindName("ScrollingHost") as ListViewBase;
-                return scrollingHost != null;
-            }
-
-            scrollingHost = null;
-            return false;
-        }
-
-        private void ProfileHeader_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            _properties.InsertScalar("ActualHeight", ProfileHeader.ActualSize.Y + 16);
-            ViewModel.HeaderHeight = Math.Max(e.NewSize.Height, 48 + 10);
-            MediaFrame.MinHeight = ScrollingHost.ActualHeight + e.NewSize.Height - 48;
-        }
-
-        private void Header_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            if (e.ClickedItem is RevenueTabItem page && page.Type != MediaFrame.Content?.GetType())
-            {
-                MediaFrame.Navigate(page.Type, null, new SuppressNavigationTransitionInfo());
-            }
-        }
-
-        private void Menu_ContextRequested(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
